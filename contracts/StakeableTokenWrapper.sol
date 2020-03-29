fd4a7547df86304bef44f99817fbf4747e7c495c
Matt Luongo <mhluongo@gmail.com>
2020-03-28 20:27:12 -0400

    Added token wrapper staking contract

---
+++ contracts/StakeableTokenWrapper.sol
@@ -0,0 +1,51 @@
+// Generalized and adapted from https://github.com/k06a/Unipool 🙇
+
+pragma solidity 0.6.4;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+/**
+ * @title StakeableTokenWrapper
+ * @notice A wrapper for an ERC-20 that can be staked and withdrawn.
+ * @dev In this contract, staked tokens don't do anything- instead other
+ * contracts can inherit from this one to add functionality.
+ */
+contract StakeableTokenWrapper {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    IERC20 public stakedToken;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+
+    constructor(IERC20 _stakedToken) public {
+        stakedToken = _stakedToken;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        stakedToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public {
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        stakedToken.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+}
---
+++ test/stakeableTokenWrapper.ts
@@ -0,0 +1,101 @@
+import { waffle } from "@nomiclabs/buidler"
+import chai from "chai"
+import { deployContract, solidity } from "ethereum-waffle"
+import { utils } from "ethers"
+
+import StakeableTokenWrapperArtifact from "../artifacts/StakeableTokenWrapper.json"
+import { StakeableTokenWrapper } from "../typechain/StakeableTokenWrapper"
+
+import MockBasicTokenArtifact from "../artifacts/MockBasicToken.json"
+import { IERC20 } from "../typechain/IERC20"
+
+chai.use(solidity)
+const { expect } = chai
+
+describe("StakeableTokenWrapper", () => {
+  const provider = waffle.provider
+  const [
+    deployer,
+    staker1,
+    staker2,
+  ] = provider.getWallets()
+
+  let basicToken: IERC20
+  let tokenWrapper: StakeableTokenWrapper
+
+  async function deployWrapper(
+    token: IERC20,
+  ) {
+    const contract = (await deployContract(
+      deployer,
+      StakeableTokenWrapperArtifact,
+      [token.address],
+    )) as StakeableTokenWrapper
+    return contract
+  }
+
+  async function approveAndStake(
+    wallet: any,
+    amount: number
+  ) {
+    const wrapperAsStaker = tokenWrapper.connect(wallet)
+    const tokenAsStaker = basicToken.connect(wallet)
+
+    await tokenAsStaker.approve(wrapperAsStaker.address, amount)
+    await wrapperAsStaker.stake(amount)
+
+    return [ wrapperAsStaker, tokenAsStaker ]
+  }
+
+  beforeEach(async () => {
+    basicToken = (await deployContract(
+      deployer,
+      MockBasicTokenArtifact,
+      [10 ** 10],
+    )) as IERC20
+
+    await basicToken.transfer(staker1.address, 1000)
+    await basicToken.transfer(staker2.address, 10000)
+
+    tokenWrapper = (await deployWrapper(
+      basicToken,
+    )) as StakeableTokenWrapper
+  })
+
+  it("Emits an event on staking", async () => {
+    let wrapperAsStaker1 = tokenWrapper.connect(staker1)
+    let tokenAsStaker1 = basicToken.connect(staker1)
+
+    await tokenAsStaker1.approve(wrapperAsStaker1.address, 1000)
+    await expect(
+      wrapperAsStaker1.stake(1000),
+    ).to.emit(tokenWrapper, 'Staked')
+  })
+
+  it("Emits an event on withdrawing", async () => {
+    const [ wrapperContract, tokenContract ] = await approveAndStake(staker1, 1000)
+
+    await expect(
+      wrapperContract.withdraw(1000),
+    ).to.emit(tokenWrapper, 'Withdrawn')
+  })
+
+  it("Only allows staked funds to be withdrawn", async () => {
+    const [ wrapperContract, tokenContract ] = await approveAndStake(staker1, 1000)
+
+    await expect(
+      wrapperContract.withdraw(1001),
+    ).to.be.reverted
+  })
+
+  it("Returns correct staked balances", async () => {
+    await approveAndStake(staker1, 1000)
+    await approveAndStake(staker2, 10000)
+
+    const balance1 = await tokenWrapper.balanceOf(staker1.address)
+    const balance2 = await tokenWrapper.balanceOf(staker2.address)
+
+    expect(balance1).to.eq(1000)
+    expect(balance2).to.eq(10000)
+  })
+})
