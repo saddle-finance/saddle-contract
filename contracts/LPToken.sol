f1c0221e4cf3e8a1d3e8aca78ea4422cd9962ee8
Matt Luongo <mhluongo@gmail.com>
2020-07-04 14:23:04 -0400

    Utilities for an LP-based stableswap contract

---
+++ contracts/LPToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LPToken is ERC20Burnable, Ownable() {
+    string private _name;
+    string private _symbol;
+
+    constructor (string memory name, string memory symbol) ERC20(name, symbol) public {}
+
+    function mint(address recipient, uint256 amount) public onlyOwner {
+        _mint(recipient, amount);
+    }
+}
---
+++ contracts/OwnerPausable.sol
@@ -0,0 +1,49 @@
+pragma solidity 0.6.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title OwnerPausable
+ * @notice An ownable contract allows the owner to pause and unpause the
+ * contract without a delay.
+ * @dev Only methods using the provided modifiers will be paused.
+ */
+contract OwnerPausable is Ownable {
+
+    event Paused();
+    event Unpaused();
+
+    bool paused = false;
+
+    /**
+     * @notice Pause the contract. Revert if already paused.
+     */
+    function pause() public onlyOwner onlyUnpaused {
+        paused = true;
+        emit Paused();
+    }
+
+    /**
+     * @notice Unpause the contract. Revert if already unpaused.
+     */
+    function unpause() public onlyOwner onlyPaused {
+        paused = false;
+        emit Unpaused();
+    }
+
+    /**
+     * @notice Revert if the contract is paused.
+     */
+    modifier onlyUnpaused() {
+        require(!paused, "Method can only be called when unpaused");
+        _;
+    }
+
+    /**
+     * @notice Revert if the contract is unpaused.
+     */
+    modifier onlyPaused() {
+        require(paused, "Method can only be called when paused");
+        _;
+    }
+}
