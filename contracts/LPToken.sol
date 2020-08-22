116afcddb84fdb751aa59dd8b16317d15f257502
Matt Luongo <mhluongo@gmail.com>
2020-08-22 14:05:56 -0400

    Remove unused name and symbol vars in LPToken
    Closes #15, closes #16.

--- contracts/LPToken.sol
+++ contracts/LPToken.sol
@@ -5,9 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LPToken is ERC20Burnable, Ownable() {
-    string private _name;
-    string private _symbol;
-
     constructor (string memory name, string memory symbol) ERC20(name, symbol) public {}
 
     function mint(address recipient, uint256 amount) public onlyOwner {
