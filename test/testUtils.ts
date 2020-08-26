f0e328ab4e86f612ff793b58dcd82a805307253f
Jongseung Lim <penandlim@gmail.com>
2020-08-26 14:43:02 -0400

    Add tests for removeLiquidity functions
    * Adds calculate functions in Swap.sol
    * Adds comments to calculateRemoveLiquidity() in SwapUtils.sol
    * Tests removeLiquidity(), removeLiquidityImbalance(), and removeLiquidityOneToken() of Swap.sol and SwapUtils.sol

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,5 +1,9 @@
-import { Bytes } from "ethers"
+import { BigNumber, Bytes, ContractFactory, Signer } from "ethers"
+import { Contract } from "@ethersproject/contracts"
 import { Artifact } from "@nomiclabs/buidler/types"
+import { ethers } from "@nomiclabs/buidler"
+
+import { Erc20 as ERC20 } from "../build/typechain/Erc20"
 
 // Workaround for linking libraries not yet working in buidler-waffle plugin
 // https://github.com/nomiclabs/buidler/issues/611
@@ -26,4 +30,40 @@ export function linkBytecode(
   }
 
   return bytecode
+}
+
+export async function deployContractWithLibraries(
+  signer: Signer,
+  artifact: Artifact,
+  libraries: Record<string, string>,
+  args?: Array<unknown>,
+): Promise<Contract> {
+  const swapFactory = (await ethers.getContractFactory(
+    artifact.abi,
+    linkBytecode(artifact, libraries),
+    signer,
+  )) as ContractFactory
+
+  if (args) {
+    return swapFactory.deploy(...args)
+  } else {
+    return swapFactory.deploy()
+  }
+}
+
+export async function getTokenBalances(
+  address: string | Signer,
+  ...tokens: ERC20[]
+): Promise<BigNumber[]> {
+  const balanceArray = []
+
+  if (address instanceof Signer) {
+    address = await address.getAddress()
+  }
+
+  for (const token of tokens) {
+    balanceArray.push(await token.balanceOf(address))
+  }
+
+  return balanceArray
 }
