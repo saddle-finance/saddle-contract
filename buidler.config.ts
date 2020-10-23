bbed46030090e34f5d6e760c610a7eefd57fe513
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2020-10-22 22:02:41 -0700

    Use the buidler-typechain plugin (#100)

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,35 +1,10 @@
-import { task, usePlugin } from "@nomiclabs/buidler/config"
+import { usePlugin } from "@nomiclabs/buidler/config"
 import { BuidlerConfig } from "@nomiclabs/buidler/config"
-import { TASK_COMPILE } from "@nomiclabs/buidler/builtin-tasks/task-names"
-import { tsGenerator } from "ts-generator"
-import { TypeChain } from "typechain/dist/TypeChain"
 
 usePlugin("@nomiclabs/buidler-ethers")
 usePlugin("@nomiclabs/buidler-waffle")
 usePlugin("solidity-coverage")
-
-task("typechain", "Generate contract typings with typechain").setAction(
-  async ({}, { config, run }) => {
-    await run(TASK_COMPILE)
-
-    console.log(`Creating Typechain artifacts in ./build/typechain/...`)
-
-    const cwd = process.cwd()
-    await tsGenerator(
-      { cwd },
-      new TypeChain({
-        cwd,
-        rawConfig: {
-          files: "./build/artifacts/*.json",
-          outDir: "./build/typechain/",
-          target: "ethers-v5",
-        },
-      }),
-    )
-
-    console.log(`Successfully generated Typechain artifacts!`)
-  },
-)
+usePlugin("buidler-typechain")
 
 const config: BuidlerConfig = {
   defaultNetwork: "buidlerevm",
@@ -44,6 +19,10 @@ const config: BuidlerConfig = {
   },
   solc: {
     version: "0.5.11",
+  },
+  typechain: {
+    outDir: "./build/typechain/",
+    target: "ethers-v5",
   },
 }
 
