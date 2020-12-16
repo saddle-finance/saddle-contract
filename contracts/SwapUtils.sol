94edfdddd2be8e8ccd50321e3d49d349bca97649
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2020-12-16 15:45:04 -0800

    [N08] Superfluous else clause
    Fix #181

--- contracts/SwapUtils.sol
+++ contracts/SwapUtils.sol
@@ -289,8 +289,6 @@ library SwapUtils {
                 // If we were to protect the division loss we would have to keep the denominator separate
                 // and divide at the end. However this leads to overflow with large numTokens or/and D.
                 // c = c * D * D * D * ... overflow!
-            } else {
-                continue;
             }
         }
         c = c.mul(D).div(nA.mul(numTokens).div(A_PRECISION));
