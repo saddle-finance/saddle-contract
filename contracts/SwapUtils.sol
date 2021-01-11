a2bf9b2ee9cbe55e828c86f9d905759ae8cd4f24
Jongseung (John) Lim <penandlim@gmail.com>
2021-01-11 14:33:59 -0500

    [M03] Approximations may finish with inaccurate values (#201)
    * Add revert call when D does not converge to values within range of 1
    
    * Add revert statements when y value does not convergei in the following
    functions
    - getYD()
    - getY()

--- contracts/SwapUtils.sol
+++ contracts/SwapUtils.sol
@@ -286,10 +286,10 @@ library SwapUtils {
             yPrev = y;
             y = y.mul(y).add(c).div(y.mul(2).add(b).sub(D));
             if(y.within1(yPrev)) {
-                break;
+                return y;
             }
         }
-        return y;
+        revert("Approximation did not converge");
     }
 
     /**
@@ -327,10 +327,14 @@ library SwapUtils {
             D = nA.mul(s).div(A_PRECISION).add(dP.mul(numTokens)).mul(D).div(
                 nA.div(A_PRECISION).sub(1).mul(D).add(numTokens.add(1).mul(dP)));
             if (D.within1(prevD)) {
-                break;
+                return D;
             }
         }
-        return D;
+
+        // Convergence should occur in 4 loops or less. If this is reached, there may be something wrong
+        // with the pool. If this were to occur repeatedly, LPs should withdraw via `removeLiquidity()`
+        // function which does not rely on D.
+        revert("D does not converge");
     }
 
     /**
@@ -459,10 +463,10 @@ library SwapUtils {
             yPrev = y;
             y = y.mul(y).add(c).div(y.mul(2).add(b).sub(D));
             if (y.within1(yPrev)) {
-                break;
+                return y;
             }
         }
-        return y;
+        revert("Approximation did not converge");
     }
 
     /**
