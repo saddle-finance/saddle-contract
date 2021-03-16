c6ce68e63827c8c977a563d58578e88040ac03de
Jongseung (John) Lim <penandlim@gmail.com>
2021-03-16 17:15:51 -0400

    Mint Dummy Tokens on Hardhat and Print Contract Addresses (#323)
    * Mint test tokens and print contract addresses
    
    * Rename token array to reflect it is for USD pool

--- deploy/110_check_USDPoolTokens.ts
+++ deploy/110_check_USDPoolTokens.ts
@@ -1,25 +1,38 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { CHAIN_ID } from "../utils/network"
+import { BigNumber } from "ethers"
 
-const BTC_TOKENS_ARGS: { [token: string]: any[] } = {
+const USD_TOKENS_ARGS: { [token: string]: any[] } = {
   DAI: ["Dai Stablecoin", "DAI", "18"],
   USDC: ["USD Coin", "USDC", "6"],
   USDT: ["Tether USD", "USDT", "6"],
 }
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
-  const { deploy } = deployments
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { deploy, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
-  for (const token in BTC_TOKENS_ARGS) {
+  for (const token in USD_TOKENS_ARGS) {
     await deploy(token, {
       from: deployer,
       log: true,
       contract: "GenericERC20",
-      args: BTC_TOKENS_ARGS[token],
+      args: USD_TOKENS_ARGS[token],
       skipIfAlreadyDeployed: true,
     })
+    // If it's on hardhat, mint test tokens
+    if ((await getChainId()) == CHAIN_ID.HARDHAT) {
+      const decimals = USD_TOKENS_ARGS[token][2]
+      await execute(
+        token,
+        { from: deployer, log: true },
+        "mint",
+        deployer,
+        BigNumber.from(10).pow(decimals).mul(1000000),
+      )
+    }
   }
 }
 export default func
