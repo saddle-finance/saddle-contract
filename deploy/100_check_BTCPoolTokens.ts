1801f70e29ab084f78dc30ef4554e8ce53ea8f3e
Jongseung Lim <penandlim@gmail.com>
2021-03-16 15:55:30 -0400

    Mint test tokens and print contract addresses

--- deploy/100_check_BTCPoolTokens.ts
+++ deploy/100_check_BTCPoolTokens.ts
@@ -1,5 +1,9 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { getChainId } from "hardhat-deploy/dist/src/utils"
+import { CHAIN_ID } from "../utils/network"
+import { DeployResult } from "hardhat-deploy/dist/types"
+import { BigNumber } from "ethers"
 
 const BTC_TOKENS_ARGS: { [token: string]: any[] } = {
   TBTC: ["tBTC", "TBTC", "18"],
@@ -9,8 +13,8 @@ const BTC_TOKENS_ARGS: { [token: string]: any[] } = {
 }
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
-  const { deploy } = deployments
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { deploy, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
   for (const token in BTC_TOKENS_ARGS) {
@@ -21,6 +25,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       args: BTC_TOKENS_ARGS[token],
       skipIfAlreadyDeployed: true,
     })
+    // If it's on hardhat, mint test tokens
+    if ((await getChainId()) == CHAIN_ID.HARDHAT) {
+      const decimals = BTC_TOKENS_ARGS[token][2]
+      await execute(
+        token,
+        { from: deployer, log: true },
+        "mint",
+        deployer,
+        BigNumber.from(10).pow(decimals).mul(10000),
+      )
+    }
   }
 }
 export default func
--- deploy/110_check_USDPoolTokens.ts
+++ deploy/110_check_USDPoolTokens.ts
@@ -1,5 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { CHAIN_ID } from "../utils/network"
+import { BigNumber } from "ethers"
 
 const BTC_TOKENS_ARGS: { [token: string]: any[] } = {
   DAI: ["Dai Stablecoin", "DAI", "18"],
@@ -8,8 +10,8 @@ const BTC_TOKENS_ARGS: { [token: string]: any[] } = {
 }
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
-  const { deploy } = deployments
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { deploy, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
   for (const token in BTC_TOKENS_ARGS) {
@@ -20,6 +22,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       args: BTC_TOKENS_ARGS[token],
       skipIfAlreadyDeployed: true,
     })
+    // If it's on hardhat, mint test tokens
+    if ((await getChainId()) == CHAIN_ID.HARDHAT) {
+      const decimals = BTC_TOKENS_ARGS[token][2]
+      await execute(
+        token,
+        { from: deployer, log: true },
+        "mint",
+        deployer,
+        BigNumber.from(10).pow(decimals).mul(1000000),
+      )
+    }
   }
 }
 export default func
