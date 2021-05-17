5a6af757fa94a9654b7f0386a1139c62de96f929
Jongseung Lim <penandlim@gmail.com>
2021-05-17 19:59:58 -0400

    Initialize target contract on deployment

--- deploy/002_deploy_LPToken.ts
+++ deploy/002_deploy_LPToken.ts
@@ -3,7 +3,7 @@ import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
-  const { deploy } = deployments
+  const { deploy, execute } = deployments
   const { libraryDeployer } = await getNamedAccounts()
 
   await deploy("LPToken", {
@@ -11,6 +11,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
     skipIfAlreadyDeployed: true,
   })
+
+  await execute(
+    "LPToken",
+    { from: libraryDeployer, log: true },
+    "initialize",
+    "Saddle LP Token (Target)",
+    "saddleLPTokenTarget",
+  )
 }
 export default func
 func.tags = ["LPToken"]
