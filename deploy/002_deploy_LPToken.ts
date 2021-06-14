6f62f3172d198efaef43d45ee5b5f6928c0412bb
Jongseung (John) Lim <penandlim@gmail.com>
2021-06-14 19:03:02 -0400

    Skip if LPToken contract is already deployed (#402)

--- deploy/002_deploy_LPToken.ts
+++ deploy/002_deploy_LPToken.ts
@@ -4,7 +4,7 @@ import { CHAIN_ID } from "../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
-  const { deploy, execute } = deployments
+  const { deploy, execute, getOrNull, log } = deployments
   const { libraryDeployer } = await getNamedAccounts()
 
   if ((await getChainId()) == CHAIN_ID.HARDHAT) {
@@ -15,19 +15,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     })
   }
 
-  await deploy("LPToken", {
-    from: libraryDeployer,
-    log: true,
-    skipIfAlreadyDeployed: true,
-  })
+  const lpToken = await getOrNull("LPToken")
+  if (lpToken) {
+    log(`reusing "LPToken" at ${lpToken.address}`)
+  } else {
+    await deploy("LPToken", {
+      from: libraryDeployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+    })
 
-  await execute(
-    "LPToken",
-    { from: libraryDeployer, log: true },
-    "initialize",
-    "Saddle LP Token (Target)",
-    "saddleLPTokenTarget",
-  )
+    await execute(
+      "LPToken",
+      { from: libraryDeployer, log: true },
+      "initialize",
+      "Saddle LP Token (Target)",
+      "saddleLPTokenTarget",
+    )
+  }
 }
 export default func
 func.tags = ["LPToken"]
