36d33c005b3ac3b6c6496bcdf528b8fa989085a9
Jongseung Lim <penandlim@gmail.com>
2021-12-10 02:39:03 -0500

    Add new deployment scripts for existing meta pools

---
+++ deploy/mainnet/260_deploy_CUSDMetaPoolUpdated.ts
@@ -0,0 +1,81 @@
+import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { execute, deploy, get, getOrNull, log, read, save } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // Manually check if the pool is already deployed
+  const saddleWCUSDMetaPool = await getOrNull("SaddleWCUSDMetaPoolUpdated")
+  if (saddleWCUSDMetaPool) {
+    log(
+      `reusing "SaddleWCUSDMetaPoolUpdated" at ${saddleWCUSDMetaPool.address}`,
+    )
+  } else {
+    // Constructor arguments
+    const TOKEN_ADDRESSES = [
+      (await get("WCUSD")).address,
+      (await get("SaddleUSDPoolV2LPToken")).address,
+    ]
+    const TOKEN_DECIMALS = [18, 18]
+    const LP_TOKEN_NAME = "Saddle wCUSD/saddleUSD-V2"
+    const LP_TOKEN_SYMBOL = "saddleWCUSD"
+    const INITIAL_A = 100
+    const SWAP_FEE = 4e6 // 4bps
+    const ADMIN_FEE = 0
+
+    const receipt = await execute(
+      "SwapDeployer",
+      {
+        from: deployer,
+        log: true,
+      },
+      "deployMetaSwap",
+      (
+        await get("SaddleSUSDMetaPoolUpdated")
+      ).address,
+      TOKEN_ADDRESSES,
+      TOKEN_DECIMALS,
+      LP_TOKEN_NAME,
+      LP_TOKEN_SYMBOL,
+      INITIAL_A,
+      SWAP_FEE,
+      ADMIN_FEE,
+      (
+        await get("LPToken")
+      ).address,
+      (
+        await get("SaddleUSDPoolV2")
+      ).address,
+    )
+
+    const newPoolEvent = receipt?.events?.find(
+      (e: any) => e["event"] == "NewSwapPool",
+    )
+    const metaSwapAddress = newPoolEvent["args"]["swapAddress"]
+    log(`deployed CUSD meta pool (targeting "MetaSwap") at ${metaSwapAddress}`)
+    await save("SaddleWCUSDMetaPoolUpdated", {
+      abi: (await get("SaddleSUSDMetaPoolUpdated")).abi,
+      address: metaSwapAddress,
+    })
+  }
+
+  const lpTokenAddress = (
+    await read("SaddleWCUSDMetaPoolUpdated", "swapStorage")
+  ).lpToken
+  log(`Saddle wCUSD MetaSwap LP Token at ${lpTokenAddress}`)
+
+  await save("SaddleWCUSDMetaPoolUpdatedLPToken", {
+    abi: (await get("LPToken")).abi, // LPToken ABI
+    address: lpTokenAddress,
+  })
+}
+export default func
+func.tags = ["WCUSDMetaPoolUpdated"]
+func.dependencies = [
+  "WCUSDMetaPoolTokens",
+  "USDPoolV2",
+  "MetaSwapUtils",
+  "AmplificationUtils",
+]
---
+++ deploy/mainnet/261_deploy_CUSDMetaPoolUpdatedDeposit.ts
@@ -0,0 +1,60 @@
+import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { execute, deploy, get, getOrNull, log, save } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // Manually check if the pool is already deployed
+  const saddleWCUSDMetaPool = await getOrNull(
+    "SaddleWCUSDMetaPoolUpdatedDeposit",
+  )
+  if (saddleWCUSDMetaPool) {
+    log(
+      `reusing "SaddleWCUSDMetaPoolUpdatedDeposit" at ${saddleWCUSDMetaPool.address}`,
+    )
+  } else {
+    const receipt = await execute(
+      "SwapDeployer",
+      {
+        from: deployer,
+        log: true,
+      },
+      "clone",
+      (
+        await get("SaddleSUSDMetaPoolDeposit")
+      ).address,
+    )
+
+    const newPoolEvent = receipt?.events?.find(
+      (e: any) => e["event"] == "NewClone",
+    )
+    const metaSwapDeposit = newPoolEvent["args"]["cloneAddress"]
+    log(
+      `deployed WCUSD meta pool deposit (targeting "MetaPoolDeposit") at ${metaSwapDeposit}`,
+    )
+    await save("SaddleWCUSDMetaPoolUpdatedDeposit", {
+      abi: (await get("SaddleSUSDMetaPoolDeposit")).abi,
+      address: metaSwapDeposit,
+    })
+
+    await execute(
+      "SaddleWCUSDMetaPoolUpdatedDeposit",
+      { from: deployer, log: true },
+      "initialize",
+      (
+        await get("SaddleUSDPoolV2")
+      ).address,
+      (
+        await get("SaddleWCUSDMetaPoolUpdated")
+      ).address,
+      (
+        await get("SaddleWCUSDMetaPoolLPToken")
+      ).address,
+    )
+  }
+}
+export default func
+func.tags = ["WCUSDMetaPoolUpdatedDeposit"]
+func.dependencies = ["WCUSDMetaPoolTokens", "WCUSDMetaPoolUpdated"]
