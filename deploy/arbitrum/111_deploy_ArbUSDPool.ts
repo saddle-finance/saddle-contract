b9d4bd6d1ec3e364cb8381f4ea2e71e5f0133ee4
Jongseung (John) Lim <penandlim@gmail.com>
2021-12-17 20:35:49 -0500

    Add deploy scripts for the FRAX,USDC,USDT pool on Arbitrum (#483)
    * Add deploy scripts for the FRAX,USDC,USDT pool on Arbitrum
    
    * Add FRAX json
    
    * Linting

--- deploy/arbitrum/111_deploy_ArbUSDPool.ts
+++ deploy/arbitrum/111_deploy_ArbUSDPool.ts
@@ -65,9 +65,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 }
 export default func
 func.tags = ["SaddleArbUSDPool"]
-func.dependencies = [
-  "SwapUtils",
-  "SwapDeployer",
-  "SwapFlashLoan",
-  "USDPoolTokens",
-]
+func.dependencies = ["SwapUtils", "SwapFlashLoan", "USDPoolTokens"]
