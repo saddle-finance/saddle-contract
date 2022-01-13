7b8b8c75f651997579be80603a0d502222935326
Jongseung Lim <penandlim@gmail.com>
2022-01-13 00:17:58 -0500

    Remove old library deployments

--- deploy/optimism/006_deploy_SwapFlashLoan.ts
+++ deploy/optimism/006_deploy_SwapFlashLoan.ts
@@ -1,23 +1,10 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
-import { isTestNetwork } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get } = deployments
   const { libraryDeployer } = await getNamedAccounts()
-
-  if (isTestNetwork(await getChainId())) {
-    await deploy("SwapFlashLoanV1", {
-      from: libraryDeployer,
-      log: true,
-      libraries: {
-        SwapUtilsV1: (await get("SwapUtilsV1")).address,
-        AmplificationUtilsV1: (await get("AmplificationUtilsV1")).address,
-      },
-      skipIfAlreadyDeployed: true,
-    })
-  }
 
   await deploy("SwapFlashLoan", {
     from: libraryDeployer,
