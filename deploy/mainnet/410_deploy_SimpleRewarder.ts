caf24b7fd5a72f930b49586fa714f4c0dce0926f
Jongseung Lim <penandlim@gmail.com>
2022-02-16 15:08:38 -0700

    Add deployment data for SimpleRewarder on mainnet

--- deploy/mainnet/410_deploy_SimpleRewarder.ts
+++ deploy/mainnet/410_deploy_SimpleRewarder.ts
@@ -2,11 +2,10 @@ import { expect } from "chai"
 import { BigNumber } from "ethers"
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { CHAIN_ID } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId, ethers } = hre
-  const { deploy, execute, get, getOrNull, save, read} = deployments
+  const { deploy, execute, get, getOrNull, save, read } = deployments
   const { deployer } = await getNamedAccounts()
 
   if ((await getOrNull("SimpleRewarder")) == null) {
@@ -20,7 +19,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     await save("SimpleRewarder_KEEP", result)
 
     const PID = 5
-    const tbtcMetaPoolLpToken = (await get("SaddleTBTCMetaPoolUpdatedLPToken")).address
+    const tbtcMetaPoolLpToken = (await get("SaddleTBTCMetaPoolUpdatedLPToken"))
+      .address
     const rewardToken = "0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC" // KEEP token
     const rewardAdmin = "0xb78c0cf4c9e9bf4ba24b17065fa8c0ac71957653" // KEEP team's OpEx wallet
     const rewardPerSecond = BigNumber.from("413359788359788360") // 250k KEEP weekly
