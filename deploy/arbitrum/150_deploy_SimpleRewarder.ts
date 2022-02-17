0573f3c5bf5196b128aa80476d71ff14e59e63a7
Jongseung Lim <penandlim@gmail.com>
2022-02-17 15:44:45 -0700

    Update SPA rewards amounts to match on chain

--- deploy/arbitrum/150_deploy_SimpleRewarder.ts
+++ deploy/arbitrum/150_deploy_SimpleRewarder.ts
@@ -22,7 +22,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const lpToken = (await get("SaddleArbUSDSMetaPoolLPToken")).address
     const rewardToken = "0x5575552988A3A80504bBaeB1311674fCFd40aD4B" // SPA token
     const rewardAdmin = "0x80a31ee7c8F9a24D7EBBE3fAFbaaF6f422307F06" // SPA team's multisig wallet
-    const rewardPerSecond = BigNumber.from("413359788359788360") // 250k KEEP weekly
+    const rewardPerSecond = BigNumber.from("1286008200000000000") // SPA reward per second
 
     // Ensure pid is correct
     expect(await read("MiniChefV2", "lpToken", PID)).to.eq(lpToken)
