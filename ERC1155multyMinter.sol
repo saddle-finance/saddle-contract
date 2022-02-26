bb2996864b5c32517ff736fe9ad143b76adb7015
George Polymenakos <76160388+UD1sto@users.noreply.github.com>
2022-02-26 07:27:34 +0000

    Create ERC1155multyMinter.sol
    Token Multyminter contract that transfers preminted balances to Distributor.sol

---
+++ ERC1155multyMinter.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract MultyTokenContract is Initializable, ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC1155_init("");
+        __Ownable_init();
+        __Pausable_init();
+        __ERC1155Burnable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    
+        public
+        onlyOwner
+    {
+        /*
+        0*oUSD
+        1*Pylon
+        2*Bak coin
+        3*Nectar
+        */
+        require (id <= 3);
+        _mint(account, id, amount, data);
+    }
+//"make"allow ids 1+2+3 , 1+2, 2+3, 1+3
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
