a048a65033fbf40cfbc6af8fd50bdf44891fea3f
Jongseung Lim <penandlim@gmail.com>
2022-03-30 07:19:54 -0400

    Add deploy script for permissionless swaps and deployer

---
+++ deploy/mainnet/450_deploy_PermissionlessSwaps.ts
@@ -0,0 +1,111 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { MULTISIG_ADDRESSES } from "../../utils/accounts"
+import { PoolRegistry } from "../../build/typechain"
+import { CHAIN_ID } from "../../utils/network"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, getChainId, ethers } = hre
+  const { deploy, get, getOrNull, execute, read } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  const permissionlessSwap = await getOrNull("PermissionlessSwapFlashLoan")
+  const permissionlessMetaSwap = await getOrNull(
+    "PermissionlessMetaSwapFlashLoan",
+  )
+  const permissionlessDeployer = await getOrNull("PermissionlessDeployer")
+  const masterRegistryAddress = (await get("MasterRegistry")).address
+
+  if (permissionlessSwap == null) {
+    await deploy("PermissionlessSwapFlashLoan", {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+      args: [masterRegistryAddress],
+      libraries: {
+        SwapUtils: (await get("SwapUtils")).address,
+        AmplificationUtils: (await get("AmplificationUtils")).address,
+      },
+    })
+  }
+
+  if (permissionlessMetaSwap == null) {
+    await deploy("PermissionlessMetaSwapFlashLoan", {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+      args: [masterRegistryAddress],
+      libraries: {
+        SwapUtils: (await get("SwapUtils")).address,
+        MetaSwapUtils: (await get("MetaSwapUtils")).address,
+        AmplificationUtils: (await get("AmplificationUtils")).address,
+      },
+    })
+  }
+
+  if (permissionlessDeployer == null) {
+    await deploy("PermissionlessDeployer", {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+      args: [
+        MULTISIG_ADDRESSES[await getChainId()],
+        (await get("MasterRegistry")).address,
+        (await get("LPToken")).address,
+        (await get("PermissionlessSwapFlashLoan")).address,
+        (await get("PermissionlessMetaSwapFlashLoan")).address,
+        (await get("SaddleSUSDMetaPoolDeposit")).address,
+      ],
+    })
+
+    await execute(
+      "MasterRegistry",
+      {
+        from: deployer,
+        log: true,
+      },
+      "addRegistry",
+      ethers.utils.formatBytes32String("PermissionlessDeployer"),
+      (
+        await get("PermissionlessDeployer")
+      ).address,
+    )
+
+    const poolRegistry: PoolRegistry = await ethers.getContract("PoolRegistry")
+
+    // 1. Grant COMMUNITY_MANAGER_ROLE to PermissionlessDeployer
+    // 2. Grant DEFAULT_ADMIN_ROLE to Multisig on this chain
+    // 3. Renounce DEFAULT_ADMIN_ROLE from deployer
+    const batchCall = [
+      await poolRegistry.populateTransaction.grantRole(
+        await poolRegistry.COMMUNITY_MANAGER_ROLE(),
+        (
+          await get("PermissionlessDeployer")
+        ).address,
+      ),
+      await poolRegistry.populateTransaction.grantRole(
+        await poolRegistry.DEFAULT_ADMIN_ROLE(),
+        MULTISIG_ADDRESSES[await getChainId()],
+      ),
+      await poolRegistry.populateTransaction.renounceRole(
+        await poolRegistry.DEFAULT_ADMIN_ROLE(),
+        deployer,
+      ),
+    ]
+
+    const batchCallData = batchCall
+      .map((x) => x.data)
+      .filter((x): x is string => !!x)
+
+    await execute(
+      "PoolRegistry",
+      { from: deployer, log: true },
+      "batch",
+      batchCallData,
+      true,
+    )
+  }
+}
+export default func
+func.tags = ["PermissionlessSwaps"]
+func.skip = async (env) => (await env.getChainId()) == CHAIN_ID.MAINNET
