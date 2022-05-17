adfc4c1ee05a4328b0eb60d2373c4c5f5cea61c3
mjdatadog <michael.daly@saddle.finance>
2022-05-17 16:56:08 -0400

    Deployed 4pool to evmos mainnet. Updated deploy script.

--- deploy/evmos/151_deploy_Evmos4Pool.ts
+++ deploy/evmos/151_deploy_Evmos4Pool.ts
@@ -1,9 +1,16 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { asyncForEach } from "../../test/testUtils"
+import { ethers } from "hardhat"
+import { GenericERC20 } from "../../build/typechain"
+
+// Contract names saved as deployments json files
+const FRAX4POOL_NAME = "SaddleEvmos4Pool"
+const FRAX4POOL_LP_TOKEN_NAME = `${FRAX4POOL_NAME}LPToken`
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
-  const { execute, get, getOrNull, log, read, save } = deployments
+  const { execute, get, getOrNull, log, read, save, deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
   // Manually check if the pool is already deployed
@@ -25,8 +32,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const SWAP_FEE = 4e6 // 4bps
     const ADMIN_FEE = 5e9 // 50% of the 4bps
 
+    // Ensure token decimals are correct before deploying
+    // Evmos explorer has some delay in updating the decimals so double check
+    await asyncForEach(TOKEN_ADDRESSES, async (tokenAddress, index) => {
+      const token = (await ethers.getContractAt(
+        "GenericERC20",
+        tokenAddress,
+      )) as GenericERC20
+      const decimals = await token.decimals()
+      if (decimals !== TOKEN_DECIMALS[index]) {
+        throw new Error(
+          `Token ${tokenAddress} has ${decimals} decimals, expected ${TOKEN_DECIMALS[index]}`,
+        )
+      } else {
+        log(`Confirmed token ${tokenAddress} has ${decimals} decimals`)
+      }
+    })
+
+    await deploy(FRAX4POOL_NAME, {
+      from: deployer,
+      log: true,
+      contract: "SwapFlashLoan",
+      libraries: {
+        SwapUtils: (await get("SwapUtils")).address,
+        AmplificationUtils: (await get("AmplificationUtils")).address,
+      },
+      skipIfAlreadyDeployed: true,
+    })
+
     await execute(
-      "SwapFlashLoan",
+      FRAX4POOL_NAME,
       { from: deployer, log: true },
       "initialize",
       TOKEN_ADDRESSES,
@@ -41,21 +76,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       ).address,
     )
 
-    await save("SaddleEvmos4pool", {
-      abi: (await get("SwapFlashLoan")).abi,
-      address: (await get("SwapFlashLoan")).address,
-    })
-
-    const lpTokenAddress = (await read("SaddleEvmos4pool", "swapStorage"))
-      .lpToken
+    const lpTokenAddress = (await read(FRAX4POOL_NAME, "swapStorage")).lpToken
     log(`Saddle Evmos 4Pool LP Token at ${lpTokenAddress}`)
 
-    await save("SaddleEvmos4poolLPToken", {
+    await save(FRAX4POOL_LP_TOKEN_NAME, {
       abi: (await get("LPToken")).abi, // LPToken ABI
       address: lpTokenAddress,
     })
   }
 }
 export default func
-func.tags = ["SaddleEvmos4pool"]
+func.tags = [FRAX4POOL_NAME]
 func.dependencies = ["SwapUtils", "SwapFlashLoan", "Evmos4poolTokens"]
