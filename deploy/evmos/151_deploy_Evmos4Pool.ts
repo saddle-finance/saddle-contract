21b9565a9e028c2f6bd4055f2fc2edb9e9c07973
mjdatadog <michael.daly@saddle.finance>
2022-05-17 11:06:09 -0400

    Added deploy script for evmos 4pool.
    Added deploy script for 4pool (DAI/USDC/USDT/FRAX) as well
    as its tokens. Added deployment for live frax deployment
    given by nomad. LP token name: "Saddle 4pool"
    LP token symbol: "saddleEvmos4pool"

---
+++ deploy/evmos/151_deploy_Evmos4Pool.ts
@@ -0,0 +1,61 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { execute, get, getOrNull, log, read, save } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // Manually check if the pool is already deployed
+  const saddleEvmos4pool = await getOrNull("SaddleEvmos4pool")
+  if (saddleEvmos4pool) {
+    log(`reusing "Evmos4poolTokens" at ${saddleEvmos4pool.address}`)
+  } else {
+    // Constructor arguments
+    const TOKEN_ADDRESSES = [
+      (await get("DAI")).address,
+      (await get("USDC")).address,
+      (await get("USDT")).address,
+      (await get("FRAX")).address,
+    ]
+    const TOKEN_DECIMALS = [18, 6, 6, 18]
+    const LP_TOKEN_NAME = "Saddle 4pool"
+    const LP_TOKEN_SYMBOL = "saddleEvmos4pool"
+    const INITIAL_A = 400
+    const SWAP_FEE = 4e6 // 4bps
+    const ADMIN_FEE = 0
+
+    await execute(
+      "SwapFlashLoan",
+      { from: deployer, log: true },
+      "initialize",
+      TOKEN_ADDRESSES,
+      TOKEN_DECIMALS,
+      LP_TOKEN_NAME,
+      LP_TOKEN_SYMBOL,
+      INITIAL_A,
+      SWAP_FEE,
+      ADMIN_FEE,
+      (
+        await get("LPToken")
+      ).address,
+    )
+
+    await save("SaddleEvmos4pool", {
+      abi: (await get("SwapFlashLoan")).abi,
+      address: (await get("SwapFlashLoan")).address,
+    })
+
+    const lpTokenAddress = (await read("SaddleEvmos4pool", "swapStorage"))
+      .lpToken
+    log(`Saddle Evmos  4Pool LP Token at ${lpTokenAddress}`)
+
+    await save("SaddleEvmos4poolLPToken", {
+      abi: (await get("LPToken")).abi, // LPToken ABI
+      address: lpTokenAddress,
+    })
+  }
+}
+export default func
+func.tags = ["SaddleEvmos4pool"]
+func.dependencies = ["SwapUtils", "SwapFlashLoan", "Evmos4poolTokens"]
