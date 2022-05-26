b85221dcf2b3ba1d255cf8e856c1686fd6211f1c
Jongseung (John) Lim <penandlim@gmail.com>
2022-05-27 00:10:58 +0900

    Use D4 pool LP token as the token to distribute to veSDL holders (#592)
    * Use D4 pool LP token as the token to distribute to veSDL holders
    
    * Add a setup script for checking fee distributor claimables

--- deploy/mainnet/507_deploy_FeeDistributor.ts
+++ deploy/mainnet/507_deploy_FeeDistributor.ts
@@ -4,8 +4,8 @@ import { MULTISIG_ADDRESSES } from "../../utils/accounts"
 import { getCurrentBlockTimestamp } from "../../test/testUtils"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, getChainId, ethers } = hre
-  const { deploy, get, getOrNull, execute, read, log } = deployments
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { deploy, get } = deployments
   const { deployer } = await getNamedAccounts()
 
   // read the current admin
@@ -17,9 +17,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       (await get("VotingEscrow")).address,
       (await get("VeSDLRewards")).address,
       await getCurrentBlockTimestamp(), // TODO: use prod timestamp
-      (
-        await get("SDL")
-      ).address, // TODO: determine which token to use for fee distribution
+      (await get("SaddleD4PoolLPToken")).address,
       deployer, // admin that can trigger clawback and kill the contract
       MULTISIG_ADDRESSES[await getChainId()], // emergency admin that receieves all of the fee tokens
     ],
