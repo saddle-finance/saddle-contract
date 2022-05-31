c0e4b57e49c624f706695c547babf24e4a38845d
mjdatadog <97006234+mjdatadog@users.noreply.github.com>
2022-05-31 05:21:08 -0400

    Deploy MiniChef to Evmos (#589)
    * Deploy script for evmos minichef.
    
    Deploying minichef contract and initially adding all live
    evmos pools.
    
    * Updated deploy script to start rewards and allocpoints at 0.
    
    Updated deploy script name to mirror deployment order. Set
    initial saddle-per-second rewards to 0. Set all allocation
    points for rewards to 0 besides initially dummy entry which
    is set to 1. Added script with needed transactions to update
    saddle-per-second and allocation points after deployment.
    Saddle-per-second after script will be set to 500_000 sdl
    over 2 months (8 weeks).
    
    * Changing Minichef deploy script SDL address.
    
    * Minichef was deployed, script created for starting rewards.
    
    * Rename deploy script
    
    * Remove past LP token from this PR
    
    * Move adding rewards to evmos minichef from scripts to deploy folder
    
    Co-authored-by: Jongseung (John) Lim <penandlim@gmail.com>

---
+++ deploy/evmos/190_deploy_MiniChefV2.ts
@@ -0,0 +1,56 @@
+import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { MiniChefV2 } from "../../build/typechain"
+import { ethers } from "hardhat"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { deploy, get, execute, getOrNull, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  const miniChef = await getOrNull("MiniChefV2")
+  if (miniChef) {
+    log(`Reusing MiniChefV2 at ${miniChef.address}`)
+  } else {
+    // Deploy retroactive vesting contract for airdrops
+    await deploy("MiniChefV2", {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+      args: [(await get("SDL")).address],
+    })
+
+    const minichef: MiniChefV2 = await ethers.getContract("MiniChefV2")
+
+    const lmRewardsPerSecond = 0
+
+    const batchCall = [
+      await minichef.populateTransaction.setSaddlePerSecond(lmRewardsPerSecond),
+      await minichef.populateTransaction.add(
+        1,
+        "0x0000000000000000000000000000000000000000", // blank lp token to enforce totalAllocPoint != 0
+        "0x0000000000000000000000000000000000000000",
+      ),
+      await minichef.populateTransaction.add(
+        0,
+        (
+          await get("SaddleEvmosFrax3PoolLPToken")
+        ).address,
+        "0x0000000000000000000000000000000000000000",
+      ),
+    ]
+
+    const batchCallData = batchCall.map((x) => x.data)
+
+    // Send batch call
+    await execute(
+      "MiniChefV2",
+      { from: deployer, log: true },
+      "batch",
+      batchCallData,
+      false,
+    )
+  }
+}
+export default func
+func.tags = ["MiniChef"]
