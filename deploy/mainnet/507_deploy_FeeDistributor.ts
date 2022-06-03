9a31dcc0e329015d752fae206dcf1179baa28fd5
Jongseung Lim <penandlim@gmail.com>
2022-06-03 22:24:03 +0900

    Deploy a dummy token for SLP that is used for FeeDistributor contract

--- deploy/mainnet/507_deploy_FeeDistributor.ts
+++ deploy/mainnet/507_deploy_FeeDistributor.ts
@@ -2,11 +2,25 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import { MULTISIG_ADDRESSES } from "../../utils/accounts"
 import { getCurrentBlockTimestamp } from "../../test/testUtils"
+import { CHAIN_ID } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get } = deployments
-  const { deployer } = await getNamedAccounts()
+  const { deployer, libraryDeployer } = await getNamedAccounts()
+
+  const chainId = await getChainId()
+
+  // If we are on hardhat, deploy a generic erc20 for the fee token.
+  if (chainId === CHAIN_ID.HARDHAT) {
+    await deploy("SushiSwapPairSDLFRAX", {
+      from: libraryDeployer,
+      contract: "GenericERC20",
+      log: true,
+      args: ["Dummy SushiSwap LP token", "testLP", 18],
+      skipIfAlreadyDeployed: true,
+    })
+  }
 
   // read the current admin
   await deploy("FeeDistributor", {
@@ -19,7 +33,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       await getCurrentBlockTimestamp(), // TODO: use prod timestamp
       (await get("SushiSwapPairSDLFRAX")).address,
       deployer, // admin that can trigger clawback and kill the contract
-      MULTISIG_ADDRESSES[await getChainId()], // emergency admin that receieves all of the fee tokens
+      MULTISIG_ADDRESSES[chainId], // emergency admin that receieves all of the fee tokens
     ],
   })
 }
