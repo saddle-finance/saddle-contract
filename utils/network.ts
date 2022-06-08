0e0296b8c172e246e9829ca4069eac8e5e9107b9
Jongseung Lim <penandlim@gmail.com>
2022-06-09 00:28:28 +0900

    Use strict equality

--- utils/network.ts
+++ utils/network.ts
@@ -16,31 +16,29 @@ export const CHAIN_ID: Record<string, string> = {
 
 export function isMainnet(networkId: string): boolean {
   return (
-    networkId == CHAIN_ID.MAINNET ||
-    networkId == CHAIN_ID.ARBITRUM_MAINNET ||
-    networkId == CHAIN_ID.OPTIMISM_MAINNET ||
-    networkId == CHAIN_ID.FANTOM_MAINNET ||
-    networkId == CHAIN_ID.EVMOS_MAINNET
+    networkId === CHAIN_ID.MAINNET ||
+    networkId === CHAIN_ID.ARBITRUM_MAINNET ||
+    networkId === CHAIN_ID.OPTIMISM_MAINNET ||
+    networkId === CHAIN_ID.FANTOM_MAINNET ||
+    networkId === CHAIN_ID.EVMOS_MAINNET
   )
 }
 
 export function isTestNetwork(networkId: string): boolean {
   return (
-    networkId == CHAIN_ID.HARDHAT ||
-    networkId == CHAIN_ID.ROPSTEN ||
-    networkId == CHAIN_ID.KOVAN ||
-    networkId == CHAIN_ID.ARBITRUM_TESTNET ||
-    networkId == CHAIN_ID.OPTIMISM_TESTNET ||
-    networkId == CHAIN_ID.FANTOM_TESTNET ||
-    networkId == CHAIN_ID.EVMOS_TESTNET ||
-    networkId == CHAIN_ID.KAVA_TESTNET
+    networkId === CHAIN_ID.HARDHAT ||
+    networkId === CHAIN_ID.ROPSTEN ||
+    networkId === CHAIN_ID.KOVAN ||
+    networkId === CHAIN_ID.ARBITRUM_TESTNET ||
+    networkId === CHAIN_ID.OPTIMISM_TESTNET ||
+    networkId === CHAIN_ID.FANTOM_TESTNET ||
+    networkId === CHAIN_ID.EVMOS_TESTNET ||
+    networkId === CHAIN_ID.KAVA_TESTNET
   )
 }
 
 export function isHardhatNetwork(networkId: string): boolean {
-  return (
-    networkId == CHAIN_ID.HARDHAT 
-  )
+  return networkId === CHAIN_ID.HARDHAT
 }
 
 export const ALCHEMY_BASE_URL = {
