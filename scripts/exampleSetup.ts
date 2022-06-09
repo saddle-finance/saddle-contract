f0d4bcc407b70bbb0eee8fba80863872d0872597
Jongseung (John) Lim <penandlim@gmail.com>
2022-06-09 11:24:08 +0900

    Cleanup example setup script and update it to recognize RootGauge (#617)

--- scripts/exampleSetup.ts
+++ scripts/exampleSetup.ts
@@ -9,6 +9,7 @@ import {
   GaugeHelperContract,
   VotingEscrow,
   Minter,
+  LiquidityGaugeV5,
 } from "../build/typechain/"
 import {
   BIG_NUMBER_1E18,
@@ -36,65 +37,33 @@ async function main() {
   const gaugeHelperContract = (await ethers.getContract(
     "GaugeHelperContract",
   )) as GaugeHelperContract
-  const gauge_map = new Map<string, string>([
-    [
-      "LiquidityGaugeV5_SaddleALETHPoolLPToken",
-      (await ethers.getContract("LiquidityGaugeV5_SaddleALETHPoolLPToken"))
-        .address,
-    ],
-    [
-      "LiquidityGaugeV5_SaddleBTCPoolV2LPToken",
-      (await ethers.getContract("LiquidityGaugeV5_SaddleBTCPoolV2LPToken"))
-        .address,
-    ],
-    [
-      "LiquidityGaugeV5_SaddleD4PoolLPToken",
-      (await ethers.getContract("LiquidityGaugeV5_SaddleD4PoolLPToken"))
-        .address,
-    ],
-    [
-      "LiquidityGaugeV5_SaddleSUSDMetaPoolUpdatedLPToken",
-      (
-        await ethers.getContract(
-          "LiquidityGaugeV5_SaddleSUSDMetaPoolUpdatedLPToken",
-        )
-      ).address,
-    ],
-    [
-      "LiquidityGaugeV5_SaddleTBTCMetaPoolUpdatedLPToken",
-      (
-        await ethers.getContract(
-          "LiquidityGaugeV5_SaddleTBTCMetaPoolUpdatedLPToken",
-        )
-      ).address,
-    ],
-    [
-      "LiquidityGaugeV5_SaddleUSDPoolV2LPToken",
-      (
-        await ethers.getContract("LiquidityGaugeV5_SaddleUSDPoolV2LPToken")
-      ).address.toString(),
-    ],
-    [
-      "LiquidityGaugeV5_SaddleWCUSDMetaPoolUpdatedLPToken",
-      (
-        await ethers.getContract(
-          "LiquidityGaugeV5_SaddleWCUSDMetaPoolUpdatedLPToken",
-        )
-      ).address.toString(),
-    ],
-  ])
-  // array of all keys in gauge_addresses
-  const gauge_names = Array.from(gauge_map.keys())
-  // array of all values in gauge_addresses
-  const gauge_addresses = Array.from(gauge_map.values())
+
+  const numberOfGauges = (await gaugeController.n_gauges()).toNumber()
+  const gauges: { name: string; address: string }[] = []
+
+  for (let i = 0; i < numberOfGauges; i++) {
+    const address = await gaugeController.gauges(i)
+    const gaugeContract = (await ethers.getContractAt(
+      "LiquidityGaugeV5",
+      address,
+    )) as LiquidityGaugeV5
+
+    let name: string
+    try {
+      name = await gaugeContract.name()
+    } catch (e) {
+      // In case of root gauges, they don't have a name.
+      name = address
+    }
+
+    gauges.push({ name, address })
+  }
 
   // The deploy scripts should have already added a default gauge type
-  expect((await gaugeController.n_gauge_types()).toNumber()).to.eq(1)
-  expect((await gaugeController.gauge_type_names(0)).toString()).to.eq("Gauge")
-
-  // The deploy scripts should have also deployed gauges for
-  // all of the existing LP tokens and added them to the gauge controller
-  expect((await gaugeController.n_gauges()).toNumber()).to.eq(7)
+  expect((await gaugeController.n_gauge_types()).toNumber()).to.eq(2)
+  expect((await gaugeController.gauge_type_names(0)).toString()).to.eq(
+    "Liquidity",
+  )
 
   // I can also console.log here
   console.log(
@@ -104,10 +73,10 @@ async function main() {
 
   // Test calling gaugeHelperContract that reads in series
   console.log(
-    "usdv2pool address: ",
-    await gaugeHelperContract.gaugeToPoolAddress(gauge_addresses[5]),
+    `${gauges[3].name} address: `,
+    await gaugeHelperContract.gaugeToPoolAddress(gauges[3].address),
   )
-  console.log(await gaugeHelperContract.gaugeToPoolData(gauge_addresses[5]))
+  console.log(await gaugeHelperContract.gaugeToPoolData(gauges[3].address))
 
   // You can freely modify timestamps and the state of the contracts to your liking.
   // For how you want to set up the contracts, please refer to test files in test/tokenomics
@@ -119,97 +88,59 @@ async function main() {
   await sdl.approve(veSDL.address, MAX_UINT256)
 
   // Create max lock with 10M SDL
-  const create_lock_gas_estimate = await veSDL.estimateGas.create_lock(
-    BIG_NUMBER_1E18.mul(10_000_000),
-    (await getCurrentBlockTimestamp()) + 4 * YEAR,
-  )
   await veSDL.create_lock(
     BIG_NUMBER_1E18.mul(10_000_000),
     (await getCurrentBlockTimestamp()) + 4 * YEAR,
-    { gasLimit: create_lock_gas_estimate },
+    { gasLimit: 3_000_000 },
   )
   console.log(
     "(10M SDL/4 Year) lock created at timestamp: ",
     await getCurrentBlockTimestamp(),
   )
 
-  // Force mine 1 block and then skip timestamp to specified time
-  await setTimestamp((await getCurrentBlockTimestamp()) + 2 * YEAR)
+  // Only manually trigger mining parameters if rate is 0 (uninitialized)
+  const rewardRate = await minter.rate()
+  if (rewardRate.eq(0)) {
+    await minter.update_mining_parameters()
+  }
 
-  await setTimestamp(
-    Math.floor(((await getCurrentBlockTimestamp()) + WEEK) / WEEK) * WEEK,
-  )
-  await minter.update_mining_parameters()
+  for (const gauge of gauges) {
+    const gaugeWeight = (
+      await gaugeController.get_gauge_weight(gauge.address, {
+        gasLimit: 3_000_000,
+      })
+    ).toString()
+    const gaugeRelativeWeight = await gaugeController[
+      "gauge_relative_weight(address)"
+    ](gauge.address, { gasLimit: 3_000_000 })
 
-  let get_gauge_weight_gas_estimate =
-    await gaugeController.estimateGas.get_gauge_weight(gauge_addresses[0])
-  let get_relative_gauge_weight_gas_estimate =
-    await gaugeController.estimateGas["gauge_relative_weight(address)"](
-      gauge_addresses[0],
-    )
-  let i
-  const change_gauge_weight_gas_estimate =
-    await gaugeController.estimateGas.change_gauge_weight(
-      gauge_addresses[0],
-      10000,
-    )
-  for (i = 0; i < gauge_names.length; i++) {
-    console.log(
-      gauge_names[i],
-      "gauge_weight: ",
-      (
-        await gaugeController.get_gauge_weight(gauge_addresses[i], {
-          gasLimit: get_gauge_weight_gas_estimate,
-        })
-      ).toString(),
-    )
-    console.log(
-      gauge_names[i],
-      "relative_weights: ",
-      (
-        await gaugeController["gauge_relative_weight(address)"](
-          gauge_addresses[i],
-          { gasLimit: get_relative_gauge_weight_gas_estimate },
-        )
-      ).toString(),
-    )
+    console.log(`${gauge.name} gauge_weight: ${gaugeWeight}`)
+    console.log(`${gauge.name} relative_weights: ${gaugeRelativeWeight}`)
 
-    // // Imitate multisig setting gauge weights
-    await gaugeController.change_gauge_weight(gauge_addresses[i], 10000, {
-      gasLimit: change_gauge_weight_gas_estimate,
+    // Imitate multisig setting gauge weights
+    await gaugeController.change_gauge_weight(gauge.address, 10000, {
+      gasLimit: 3_000_000,
     })
   }
 
-  // // Skip to the week after when the weights apply
+  // Skip to the week after when the weights apply
   await setTimestamp(
     Math.floor(((await getCurrentBlockTimestamp()) + WEEK) / WEEK) * WEEK,
   )
   console.log("timestamp set at", await getCurrentBlockTimestamp())
-  get_gauge_weight_gas_estimate =
-    await gaugeController.estimateGas.get_gauge_weight(gauge_addresses[0])
-  get_relative_gauge_weight_gas_estimate = await gaugeController.estimateGas[
-    "gauge_relative_weight(address)"
-  ](gauge_addresses[0])
-  for (i = 0; i < gauge_names.length; i++) {
-    console.log(
-      gauge_names[i],
-      "gauge_weight: ",
-      (
-        await gaugeController.get_gauge_weight(gauge_addresses[i], {
-          gasLimit: get_gauge_weight_gas_estimate,
-        })
-      ).toString(),
-    )
-    console.log(
-      gauge_names[i],
-      "relative_weights: ",
-      (
-        await gaugeController["gauge_relative_weight(address)"](
-          gauge_addresses[i],
-          { gasLimit: get_relative_gauge_weight_gas_estimate },
-        )
-      ).toString(),
-    )
+
+  for (const gauge of gauges) {
+    const gaugeWeight = (
+      await gaugeController.get_gauge_weight(gauge.address, {
+        gasLimit: 3_000_000,
+      })
+    ).toString()
+    const gaugeRelativeWeight = await gaugeController[
+      "gauge_relative_weight(address)"
+    ](gauge.address, { gasLimit: 3_000_000 })
+
+    console.log(`${gauge.name} gauge_weight: ${gaugeWeight}`)
+    console.log(`${gauge.name} relative_weights: ${gaugeRelativeWeight}`)
   }
 }
 
