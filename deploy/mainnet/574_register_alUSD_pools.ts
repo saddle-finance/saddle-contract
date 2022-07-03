582eb8cefe0a8cb62fa7dfc1b1a940415589874e
Michael Daly <dalymike102@gmail.com>
2022-07-03 19:37:44 -0400

    registered new alUSD pool on mainnnet and fantom

---
+++ deploy/mainnet/574_register_alUSD_pools.ts
@@ -0,0 +1,51 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { PoolRegistry } from "../../build/typechain"
+import { PoolType } from "../../utils/constants"
+import { IPoolRegistry } from "../../build/typechain"
+import { ZERO_ADDRESS } from "../../test/testUtils"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, ethers } = hre
+  const { get, execute, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  const poolRegistry: PoolRegistry = await ethers.getContract("PoolRegistry")
+
+  const pools: IPoolRegistry.PoolInputDataStruct[] = [
+    {
+      // frax alUSD meta Pool
+      poolAddress: (await get("SaddleFRAXalUSDMetaPool")).address,
+      typeOfAsset: PoolType.USD,
+      poolName: ethers.utils.formatBytes32String("FRAXBP-alUSD"),
+      targetAddress: (await get("SaddleFRAXalUSDMetaPool")).address,
+      metaSwapDepositAddress: (await get("SaddleFRAXalUSDMetaPoolDeposit"))
+        .address,
+      isSaddleApproved: true,
+      isRemoved: false,
+      isGuarded: false,
+    },
+  ]
+
+  log("Adding pools to registry")
+  const batchCall = await Promise.all(
+    pools.map(
+      async (pool) => await poolRegistry.populateTransaction.addPool(pool),
+    ),
+  )
+  const batchCallData = batchCall.map((x) => x.data).filter(Boolean)
+
+  await execute(
+    "PoolRegistry",
+    {
+      from: deployer,
+      log: true,
+      nonce: 322,
+      maxFeePerGas: ethers.utils.parseUnits("21", "gwei"),
+    },
+    "batch",
+    batchCallData,
+    true,
+  )
+}
+export default func
