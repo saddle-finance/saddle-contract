8840887555ae1c2b4391f3ad2980431d4aaefd34
Michael Daly <dalymike102@gmail.com>
2022-07-15 13:30:32 -0400

    added hardhat-deploy verification and pool registry

--- deploy/kava_mainnet/999_verify_all_contracts.ts
+++ deploy/kava_mainnet/999_verify_all_contracts.ts
@@ -3,11 +3,35 @@ import { DeployFunction } from "hardhat-deploy/types"
 import { CHAIN_ID } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getChainId } = hre
+  const { deployments, getChainId, ethers } = hre
   const { log } = deployments
+  const contracts: string[] = [
+    "LPToken",
+    "AmplificationUtils",
+    "SwapUtils",
+    "MetaSwapUtils",
+    "SwapFlashLoan",
+    "Saddle3Pool",
+    "Muticall",
+    "Muticall2",
+    "Muticall3",
+  ]
 
   if ((await getChainId()) === CHAIN_ID.KAVA_MAINNET) {
-    await hre.run("etherscan-verify")
+    for (let i = 0; i < contracts.length; i++) {
+      const contractName = contracts[i]
+      const contract = await ethers.getContract(contracts[i])
+      try {
+        await hre.run("etherscan-verify", {
+          contractName: contractName,
+        })
+        console.log(
+          `Successfully verified ${contractName} at ${contract.address}`,
+        )
+      } catch (error) {
+        console.log("verification failed with: ", error)
+      }
+    }
   } else {
     log(
       `Skipping verification since this is not running on ${CHAIN_ID.KAVA_MAINNET}`,
