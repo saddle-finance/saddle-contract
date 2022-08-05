3621b3754bb37dd00564967024222346e18202d6
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2022-08-05 11:37:32 -0700

    beginning to refactor code

--- certora/harness/SwapHarness.sol
+++ certora/harness/SwapHarness.sol
@@ -7,9 +7,10 @@ import "../munged/Swap.sol";
 // methods for the spec to access internal state, or may override some of the
 // more complex methods in the original contract.
 contract SwapHarness is Swap {
-    function isInitialized() public view returns (bool) {
+    
+    /*function isInitialized() public view returns (bool) {
         return _initialized;
-    }
+    }*/
     
     function inRampA() public view returns (bool) {
         return (block.timestamp >= swapStorage.initialATime &&
@@ -18,6 +19,38 @@ contract SwapHarness is Swap {
     }
 
     function getPooledTokenAddress(uint256 index) public view returns (address) {
-        return address(getToken(index));
-    } 
+        return address(getToken(uint8(index)));
+    }
+
+    // refactoring addLiquidity in SwapUtils
+
+    function addLiquidityHelper1(ManageLiquidityInfo memory v, IERC20[] memory pooledTokens,uint256[] memory amounts) public returns(uint256[] memory newBalances) {
+        uint256[] memory newBalances = new uint256[](pooledTokens.length);
+
+        for (uint256 i = 0; i < pooledTokens.length; i++) {
+            require(
+                v.totalSupply != 0 || amounts[i] > 0,
+                "Must supply all tokens in pool"
+            );
+
+            // Transfer tokens first to see if a fee was charged on transfer
+            if (amounts[i] != 0) {
+                uint256 beforeBalance = pooledTokens[i].balanceOf(
+                    address(this)
+                );
+                pooledTokens[i].safeTransferFrom(
+                    msg.sender,
+                    address(this),
+                    amounts[i]
+                );
+
+                // Update the amounts[] with actual transfer amount
+                amounts[i] = pooledTokens[i].balanceOf(address(this)).sub(
+                    beforeBalance
+                );
+            }
+
+            newBalances[i] = v.balances[i].add(amounts[i]);
+        }
+    }
 }
