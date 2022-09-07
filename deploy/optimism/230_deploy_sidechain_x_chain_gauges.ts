ab901b1fe4c8c5a559fa8adde039b160e697ff67
Jongseung Lim <penandlim@gmail.com>
2022-09-07 14:23:30 -0400

    Add deploy scripts

--- deploy/optimism/230_deploy_sidechain_x_chain_gauges.ts
+++ deploy/optimism/230_deploy_sidechain_x_chain_gauges.ts
@@ -1,12 +1,16 @@
+import { tracer } from "hardhat"
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { AnyCallTranslator, AnyCallExecutor } from "../../build/typechain"
 import {
   BIG_NUMBER_1E18,
+  convertGaugeNameToSalt,
   impersonateAccount,
   setEtherBalance,
   ZERO_ADDRESS,
 } from "../../test/testUtils"
 import { ANYCALL_ADDRESS, PROD_DEPLOYER_ADDRESS } from "../../utils/accounts"
+import { CHAIN_ID } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, ethers } = hre
@@ -101,7 +105,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     // Add to RootGaugeFactory to master registry
     const prodDeployer = await impersonateAccount(PROD_DEPLOYER_ADDRESS)
     await setEtherBalance(PROD_DEPLOYER_ADDRESS, BIG_NUMBER_1E18.mul(10000))
-
     const mr = await ethers.getContract("MasterRegistry")
     await mr
       .connect(prodDeployer)
@@ -109,6 +112,50 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         ethers.utils.formatBytes32String("ChildGaugeFactory"),
         cgf.address,
       )
+
+    // Add CGF to addKnownCallers from owner
+    const anyCallTranslatorProxyContract: AnyCallTranslator = await ethers.getContractAt("AnyCallTranslator", anyCallTranslatorProxy.address)
+    await anyCallTranslatorProxyContract.connect(await impersonateAccount(owner)).addKnownCallers([cgf.address])
+
+    // Deploy a child gauge for testing
+    // Impersonate AnyCall.executor() and call AnyCallTranslatorProxy.anyExecute()
+    // with correct calldata for creating a new child gauge
+    // For testing purposes, we will create a child gauge for SaddleFRAXBPPool
+    const executorAddress = await ethers.getContractAt("MockAnyCall", ANYCALL_ADDRESS).then((c) => c.executor())
+    const executorContract: AnyCallExecutor = await ethers.getContractAt("AnyCallExecutor", executorAddress)
+    const executorCreatorAddress = await executorContract.creator()
+    const executorCreator = await impersonateAccount(executorCreatorAddress)
+    await setEtherBalance(executorCreatorAddress, BIG_NUMBER_1E18.mul(10000))
+
+    // Format deploy_gauge call data that will be passed from RootGaugeFactory
+    let callData = await ethers
+      .getContractFactory("ChildGaugeFactory")
+      .then(async (c) =>
+        c.interface.encodeFunctionData("deploy_gauge(address,bytes32,string,address)",
+        [
+          (await get("SaddleFRAXBPPoolLPToken")).address, // LP token address
+          convertGaugeNameToSalt("FraxBP X-Chain Gauge"), // salt
+          "FraxBP X-Chain Gauge", // name
+          deployer // manager of the gauge
+        ]
+          ),
+      )
+    
+    // Format additional calldata for calling AnyCallTranslatorProxy.anyExecute()
+    callData = ethers.utils.defaultAbiCoder.encode(
+      ["address", "bytes"],
+      [cgf.address, callData],
+    )
+
+
+    // Call anyExecute from impersonated executor account (owned by AnyCall)
+    await executorContract.connect(executorCreator).execute(
+      anyCallTranslatorProxy.address,
+      callData,
+      anyCallTranslatorProxy.address, // Pretend the call came from same address from source chain
+      CHAIN_ID.MAINNET, // Source chain ID
+      0, // Source nonce
+    )
   } else {
     log(`Not running on forked mode, skipping`)
   }
