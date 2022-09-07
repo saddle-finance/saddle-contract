80a44d572d94c81fdae5dad89dd6348c8937e9db
Jongseung Lim <penandlim@gmail.com>
2022-09-07 17:51:56 -0400

    Adds `patch-package` to apply fix for Arbitrum Nitro

--- deploy/arbitrum/999_impersonate_owners.ts
+++ deploy/arbitrum/999_impersonate_owners.ts
@@ -27,26 +27,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // key = token deployment name, value = array of addresses
   const tokenToAccountsMap: Record<string, string[]> = {
     // USD
-    DAI: ["0xa5407eae9ba41422680e2e00537571bcc53efbfd"],
-    USDC: ["0xa5407eae9ba41422680e2e00537571bcc53efbfd"],
-    USDT: ["0xa5407eae9ba41422680e2e00537571bcc53efbfd"],
-    SUSD: ["0xa5407eae9ba41422680e2e00537571bcc53efbfd"],
-    // BTC
-    WBTC: ["0x7fc77b5c7614e1533320ea6ddc2eb61fa00a9714"],
-    RENBTC: ["0x7fc77b5c7614e1533320ea6ddc2eb61fa00a9714"],
-    SBTC: ["0x7fc77b5c7614e1533320ea6ddc2eb61fa00a9714"],
-    TBTC: ["0xC25099792E9349C7DD09759744ea681C7de2cb66"],
-    // ETH
-    ALETH: ["0x1d2c4cd9bee9dfe088430b95d274e765151c32db"],
-    WETH: ["0xceff51756c56ceffca006cd410b03ffc46dd3a58"],
-    SETH: ["0xc5424b857f758e906013f3555dad202e4bdb4567"],
-    // D4
-    ALUSD: ["0x43b4fdfd4ff969587185cdb6f0bd875c5fc83f8c"],
-    FEI: ["0x94b0a3d511b6ecdb17ebf877278ab030acb0a878"],
-    FRAX: ["0xd632f22692fac7611d2aa1c0d552930d43caed3b"],
-    LUSD: ["0x66017d22b0f8556afdd19fc67041899eb65a21bb"],
-    // TBTC
-    TBTCv2: ["0xf9e11762d522ea29dd78178c9baf83b7b093aacc"],
+    USDC: ["0x489ee077994B6658eAfA855C308275EAd8097C4A"],
+    FRAX: ["0x489ee077994b6658eafa855c308275ead8097c4a"],
+    USDT: ["0x489ee077994b6658eafa855c308275ead8097c4a"],
   }
 
   if (
