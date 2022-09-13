05065eda5b42be2bce7e521dcf7b880adf36cffe
Jongseung (John) Lim <penandlim@gmail.com>
2022-09-13 02:17:22 -0400

    Update hardhat to 2.11.1 and associated dependencies (#704)
    * Update hardhat to 2.11.1 and associated dependencies
    
    * Update solidity-coverage package version
    
    * Update tests to use the ignore tx type flag

--- test/metaPool_Inflated_BP_VP.ts
+++ test/metaPool_Inflated_BP_VP.ts
@@ -54,7 +54,7 @@ describe("Meta-Swap with inflated baseVirtualPrice", async () => {
   const setupTest = deployments.createFixture(
     async ({ deployments, ethers }) => {
       const { get } = deployments
-      await deployments.fixture() // ensure you start from a fresh deployments
+      await deployments.fixture(["Swap", "USDPool"]) // ensure you start from a fresh deployments
 
       signers = await ethers.getSigners()
       owner = signers[0]
