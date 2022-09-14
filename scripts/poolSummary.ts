0952a99aaa39e35358ffee397e75cdb562aba575
Michael Daly <dalymike102@gmail.com>
2022-09-14 16:48:01 -0400

    optional arg for specifiying multiple networks to run on

--- scripts/poolSummary.ts
+++ scripts/poolSummary.ts
@@ -17,27 +17,14 @@ async function main() {
   await logNetworkDetails(ethers.provider, network)
 
   // const signers = await ethers.getSigners()
-  const poolReg = await ethers.getContract("PoolRegistry")
-  console.log(await poolReg.getPoolDataAtIndex(1))
+  const poolRegistry = (await ethers.getContract(
+    "PoolRegistry",
+  )) as PoolRegistry
 
-  // const poolRegistry = (await ethers.getContract(
-  //   "PoolRegistry",
-  // )) as PoolRegistry
-
-  // const sdl = (await ethers.getContract("SDL")) as SDL
-  // const veSDL = (await ethers.getContract("VotingEscrow")) as VotingEscrow
-  // const minter = (await ethers.getContract("Minter")) as Minter
-  // const feeDistributor = (await ethers.getContract(
-  //   "FeeDistributor",
-  // )) as FeeDistributor
-
-  // // get all pools from pool registry
-  // let poolRegLegnth: number = (await poolRegistry.getPoolsLength()).toNumber()
-  // const lastpool = await poolRegistry.getPoolDataAtIndex(poolRegLegnth - 1)
-  // console.log(lastpool)
-  // const DAY = 86400
-  // const WEEK = 86400 * 7
-  // const YEAR = WEEK * 52
+  // get all pools from pool registry
+  let poolRegLegnth: number = (await poolRegistry.getPoolsLength()).toNumber()
+  const lastpool = await poolRegistry.getPoolDataAtIndex(poolRegLegnth - 1)
+  console.log(lastpool)
 }
 main()
   .then(() => process.exit(0))
--- tasks/runAll.ts
+++ tasks/runAll.ts
@@ -1,27 +1,45 @@
 import "hardhat-deploy"
-import config from "../hardhat.config"
 import { subtask, task, types } from "hardhat/config"
-import { NetworkUserConfig } from "hardhat/types"
+import config from "../hardhat.config"
 require("@nomicfoundation/hardhat-toolbox")
 
 task("runAll", "Runs Script against all configured networks")
-  .addParam("script", "The name of the script within the scripts folder to run")
+  .addParam(
+    "script",
+    "The name of the script within the scripts folder to run",
+    undefined,
+    types.string,
+  )
+  .addOptionalParam(
+    "networks",
+    "The name of the network to include the deployments from. Only valid when running with --network localhost",
+    undefined,
+    types.string,
+  )
   .setAction(async (taskArgs, hre) => {
-    console.log("taskargs", taskArgs)
-    for (let key in config.networks) {
-      if (key.includes("mainnet")) {
-        hre.hardhatArguments.network = key
-        console.log(`Connected to ${key}`)
-        try {
-          await hre.run("run", {
-            noCompile: false,
-            includeNetwork: undefined,
-            script: `scripts/${taskArgs.script}`,
-          })
-        } catch (e) {
-          console.log(`script failed on ${key} network with: `)
-          console.log(e)
+    let networksToRun: string[] = []
+    if (taskArgs.networks) {
+      console.log(taskArgs.networks)
+      networksToRun = taskArgs.networks.split(",")
+    } else {
+      for (let key in config.networks) {
+        if (key.includes("mainnet")) {
+          networksToRun.push(key)
         }
+      }
+    }
+    for (let i in networksToRun) {
+      const currentNetwork = networksToRun[i]
+      console.log(`Running script on network ${currentNetwork}`)
+      hre.hardhatArguments.network = currentNetwork
+      try {
+        await hre.run("run", {
+          noCompile: false,
+          includeNetwork: undefined,
+          script: `scripts/${taskArgs.script}`,
+        })
+      } catch (e) {
+        console.error(`script failed on ${currentNetwork} network with: `, e)
       }
     }
   })
