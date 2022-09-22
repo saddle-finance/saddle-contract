8cbf1eaa4159f7ab5468b941822835de58b7a4e6
Jongseung Lim <penandlim@gmail.com>
2022-09-21 20:35:52 -0400

    Update ArbitrumBridger.sol to Nitro version. Increase coverage

--- contracts/xchainGauges/bridgers/ArbitrumBridger.sol
+++ contracts/xchainGauges/bridgers/ArbitrumBridger.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.17;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts-4.2.0/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-4.4.0/token/ERC20/utils/SafeERC20.sol";
 
 interface IGatewayRouter {
     function getGateWay(address _token) external view returns (address);
+
+    function outboundTransferCustomRefund(
+        address _l1Token,
+        address _refundTo,
+        address _to,
+        uint256 _amount,
+        uint256 _maxGas,
+        uint256 _gasPriceBid,
+        bytes calldata _data
+    ) external payable;
 
     function outboundTransfer(
         address _token,
@@ -16,6 +26,21 @@ interface IGatewayRouter {
         uint256 _gasPriceBid,
         bytes calldata _data // _max_submission_cost, _extra_data
     ) external payable;
+
+    function getOutboundCalldata(
+        address _token,
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes calldata _data
+    ) external view returns (uint256, uint256);
+}
+
+interface Inbox {
+    function calculateRetryableSubmissionFee(
+        uint256 _data_length,
+        uint256 _base_fee
+    ) external view returns (uint256);
 }
 
 contract ArbitrumBridger {
@@ -26,13 +51,9 @@ contract ArbitrumBridger {
         0xa3A7B6F88361F48403514059F1F16C8E78d60EeC;
     address private constant ARB_GATEWAY_ROUTER =
         0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef;
-    uint256 private constant MAX_UINT256 = 2**256 - 1;
-    address private constant ZERO_ADDRESS =
-        0x0000000000000000000000000000000000000000;
+    address private constant INBOX = 0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f;
     // vars
-    uint256 private gasLimit;
-    uint256 private gasPrice;
-    uint256 private maxSubmissionCost;
+    uint256 private submissionData;
 
     mapping(address => bool) public approved;
 
@@ -45,33 +66,32 @@ contract ArbitrumBridger {
     event TransferOwnership(address oldOwner, address newOwner);
 
     event UpdateSubmissionData(
-        uint256[3] oldSubmissionData,
-        uint256[3] newSubmissionData
+        uint256[2] oldSubmissionData,
+        uint256[2] newSubmissionData
     );
 
     constructor(
         uint256 _gasLimit,
         uint256 _gasPrice,
-        uint256 _maxSubmissionCost,
         address _SDL
     ) {
         SDL = _SDL;
         // construct submission data
-        gasLimit = _gasLimit;
-        gasPrice = _gasPrice;
-        maxSubmissionCost = _maxSubmissionCost;
+        require(_gasLimit < type(uint128).max && _gasPrice < type(uint128).max);
+        submissionData = (_gasLimit << 128) + _gasPrice;
         emit UpdateSubmissionData(
-            [uint256(0), uint256(0), uint256(0)],
-            [gasLimit, gasLimit, maxSubmissionCost]
+            [uint256(0), uint256(0)],
+            [_gasLimit, _gasPrice]
         );
 
         // approve token transfer to gateway
-        IERC20 sdlToken = IERC20(SDL);
-        // TODO: doesn't allow for safeApprove?
-        assert(sdlToken.approve(ARB_GATEWAY, MAX_UINT256));
-        approved[SDL] = true;
+        IERC20 sdlToken = IERC20(_SDL);
+        sdlToken.safeApprove(ARB_GATEWAY, type(uint256).max);
+        approved[_SDL] = true;
+
+        // update owner
         owner = msg.sender;
-        emit TransferOwnership(ZERO_ADDRESS, msg.sender);
+        emit TransferOwnership(address(0), msg.sender);
     }
 
     function bridge(
@@ -79,48 +99,74 @@ contract ArbitrumBridger {
         address _to,
         uint256 _amount
     ) external payable {
-        // TODO: doesn't allow for safeTransferFrom?
-        assert(IERC20(_token).transferFrom(msg.sender, address(this), _amount));
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+
         if (_token != SDL && !approved[_token]) {
-            // TODO: doesn't allow for safeApprove?
-            assert(
-                IERC20(_token).approve(
-                    IGatewayRouter(ARB_GATEWAY_ROUTER).getGateWay(SDL),
-                    MAX_UINT256
-                )
+            IERC20(_token).safeApprove(
+                IGatewayRouter(ARB_GATEWAY_ROUTER).getGateWay(_token),
+                type(uint256).max
             );
             approved[_token] = true;
         }
-        IGatewayRouter(ARB_GATEWAY_ROUTER).outboundTransfer{
-            value: gasLimit * gasPrice + maxSubmissionCost
+
+        uint256 gasLimit = submissionData >> 128;
+        uint256 gasPrice = submissionData & type(uint128).max;
+        uint256 outboundCalldata;
+        (, outboundCalldata) = IGatewayRouter(ARB_GATEWAY_ROUTER)
+            .getOutboundCalldata(_token, address(this), _to, _amount, "");
+        uint256 submissionCost = Inbox(INBOX).calculateRetryableSubmissionFee(
+            outboundCalldata + 256,
+            block.basefee
+        );
+
+        IGatewayRouter(ARB_GATEWAY_ROUTER).outboundTransferCustomRefund{
+            value: gasLimit * gasPrice + submissionCost
         }(
             _token,
+            owner,
             _to,
             _amount,
             gasLimit,
             gasPrice,
-            abi.encode(maxSubmissionCost, new bytes(0))
+            abi.encode(submissionCost, "")
         );
+
+        // Send any remaining ETH to the owner
+        if (address(this).balance != 0) {
+            payable(owner).transfer(address(this).balance);
+        }
     }
 
     function cost() external view returns (uint256) {
-        // gasLimit * gasPrice + maxSubmissionCost
-        return (gasLimit * gasPrice + maxSubmissionCost);
+        return cost(block.basefee);
     }
 
-    function setSubmissionData(
-        uint256 _gasLimit,
-        uint256 _gasPrice,
-        uint256 _maxSubmissionCost
-    ) external {
-        require(msg.sender == owner, "error msg");
-        emit UpdateSubmissionData(
-            [gasLimit, gasPrice, maxSubmissionCost],
-            [_gasLimit, _gasPrice, _maxSubmissionCost]
+    function cost(uint256 basefee) public view returns (uint256) {
+        // gasLimit * gasPrice + maxSubmissionCost
+        uint256 outboundCalldata;
+        (, outboundCalldata) = IGatewayRouter(ARB_GATEWAY_ROUTER)
+            .getOutboundCalldata(SDL, address(this), msg.sender, 10**36, "");
+        console.log("outboundCalldata %s", outboundCalldata);
+        console.log("blockfee %s", basefee);
+        uint256 submissionCost = Inbox(INBOX).calculateRetryableSubmissionFee(
+            outboundCalldata + 256,
+            basefee
         );
-        gasLimit = _gasLimit;
-        gasPrice = _gasPrice;
-        maxSubmissionCost = _maxSubmissionCost;
+        console.log(submissionCost);
+        uint256 data = submissionData;
+        return ((data >> 128) * (data & type(uint128).max) + submissionCost);
+    }
+
+    function setSubmissionData(uint256 _gasLimit, uint256 _gasPrice) external {
+        require(msg.sender == owner, "error msg");
+        // construct submission data
+        require(_gasLimit < type(uint128).max && _gasPrice < type(uint128).max);
+        uint256 data = submissionData;
+        submissionData = (_gasLimit << 128) + _gasPrice;
+        emit UpdateSubmissionData(
+            [data >> 128, data & type(uint128).max],
+            [_gasLimit, _gasPrice]
+        );
     }
 
     function commitTransferOwnership(address _futureOwner) external {
--- test/xchainGauges/arbitrumBridger.ts
+++ test/xchainGauges/arbitrumBridger.ts
@@ -1,7 +1,16 @@
+import { setNextBlockBaseFeePerGas } from "@nomicfoundation/hardhat-network-helpers"
 import chai from "chai"
 import { Signer } from "ethers"
-import { deployments } from "hardhat"
-import { ArbitrumBridger } from "../../build/typechain"
+import { companionNetworks, deployments, network } from "hardhat"
+import { ArbitrumBridger, SDL } from "../../build/typechain"
+import { MULTISIG_ADDRESSES } from "../../utils/accounts"
+import { ALCHEMY_BASE_URL, CHAIN_ID } from "../../utils/network"
+import {
+  BIG_NUMBER_1E18,
+  impersonateAccount,
+  MAX_UINT256,
+  setEtherBalance,
+} from "../testUtils"
 
 const { execute } = deployments
 
@@ -10,31 +19,46 @@ const { expect } = chai
 describe("ArbitrumBridger", () => {
   let signers: Array<Signer>
   let users: string[]
-  let user1: Signer
   let arbitrumBridger: ArbitrumBridger
+  let sdl: SDL
 
   const gasLimit = 1000000
   const gasPrice = 990000000
-  const maxSubmissionCost = 10000000000000
+  const TEST_ADDRESS = "0x00000000000000000000000000000000DeaDBeef"
 
   const setupTest = deployments.createFixture(
     async ({ deployments, ethers }) => {
-      await deployments.fixture(["veSDL"], { fallbackToGlobal: false }) // ensure you start from a fresh deployments
+      await network.provider.request({
+        method: "hardhat_reset",
+        params: [
+          {
+            forking: {
+              jsonRpcUrl:
+                ALCHEMY_BASE_URL[CHAIN_ID.MAINNET] +
+                process.env.ALCHEMY_API_KEY,
+              ignoreUnknownTxType: true,
+            },
+          },
+        ],
+      })
 
       signers = await ethers.getSigners()
-      user1 = signers[1]
       users = await Promise.all(
         signers.map(async (signer) => signer.getAddress()),
+      )
+
+      sdl = await ethers.getContractAt(
+        "SDL",
+        (
+          await companionNetworks["mainnet"].deployments.get("SDL")
+        ).address,
       )
 
       const bridgerFactory = await ethers.getContractFactory("ArbitrumBridger")
       arbitrumBridger = (await bridgerFactory.deploy(
         gasLimit,
         gasPrice,
-        maxSubmissionCost,
-        (
-          await ethers.getContract("SDL")
-        ).address,
+        sdl.address,
       )) as ArbitrumBridger
     },
   )
@@ -43,11 +67,134 @@ describe("ArbitrumBridger", () => {
     await setupTest()
   })
 
-  describe("Arbitrum Bridger", () => {
-    it(`Successfully initializes with cost`, async () => {
-      expect(await arbitrumBridger.cost()).to.eq(
-        gasLimit * gasPrice + maxSubmissionCost,
+  after(async () => {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [],
+    })
+  })
+
+  describe("cost", () => {
+    it(`Returns correct estimation for gas cost`, async () => {
+      // Provide base fee for hardhat workaround
+      // https://github.com/NomicFoundation/hardhat/issues/1688
+      expect(await arbitrumBridger["cost(uint256)"]("10653818828")).to.eq(
+        "1068582567675328",
       )
+    })
+
+    it(`Returns lower value when basefee is zero`, async () => {
+      // https://github.com/NomicFoundation/hardhat/issues/1688
+      // Since basefee is 0 on view functions, we expect to see lower value than above
+      expect(await arbitrumBridger["cost()"]()).to.eq("990000000000000")
+    })
+  })
+
+  describe("setSubmissionData", () => {
+    it(`Emits UpdateSubmissionData event and sets submission data`, async () => {
+      const newGasLimit = 2000000
+      const newGasPrice = 990000000
+      await expect(arbitrumBridger.setSubmissionData(newGasLimit, newGasPrice))
+        .to.emit(arbitrumBridger, "UpdateSubmissionData")
+        .withArgs([gasLimit, gasPrice], [newGasLimit, newGasPrice])
+    })
+  })
+
+  describe("commitTransferOwnership", () => {
+    it(`Reverts when not called by the owner`, async () => {
+      await expect(
+        arbitrumBridger
+          .connect(signers[1])
+          .commitTransferOwnership(TEST_ADDRESS),
+      ).to.be.reverted
+    })
+
+    it(`Successfully sets futureOwner`, async () => {
+      await arbitrumBridger.commitTransferOwnership(TEST_ADDRESS)
+      expect(await arbitrumBridger.futureOwner()).to.eq(TEST_ADDRESS)
+    })
+  })
+
+  describe("acceptTransferOwnership", () => {
+    it(`Reverts when not called by the futureOwner`, async () => {
+      await expect(
+        arbitrumBridger.connect(signers[1]).acceptTransferOwnership(),
+      ).to.be.reverted
+    })
+
+    it(`Successfully transfers ownership to futureOwner`, async () => {
+      await arbitrumBridger.commitTransferOwnership(users[10])
+      await expect(
+        arbitrumBridger.connect(signers[10]).acceptTransferOwnership(),
+      )
+        .to.emit(arbitrumBridger, "TransferOwnership")
+        .withArgs(users[0], users[10])
+      expect(await arbitrumBridger.owner()).to.eq(users[10])
+    })
+  })
+
+  describe("bridge", () => {
+    it(`Successfully Sends SDL to Arbitrum Router`, async () => {
+      // Give some SDL to users[0]
+      const sdlHolder = await impersonateAccount(
+        MULTISIG_ADDRESSES[CHAIN_ID.MAINNET],
+      )
+      await setEtherBalance(
+        await sdlHolder.getAddress(),
+        BIG_NUMBER_1E18.mul(100),
+      )
+      await sdl
+        .connect(sdlHolder)
+        .transfer(users[0], BIG_NUMBER_1E18.mul(10000))
+
+      // Approve bridger to use users[0]'s SDL
+      await sdl.approve(arbitrumBridger.address, MAX_UINT256)
+      // Set base fee for consistent test result
+      await setNextBlockBaseFeePerGas(10653818828)
+      // Expect the bridge call to successfully transfer SDL token to the router
+      await expect(
+        arbitrumBridger.bridge(
+          sdl.address,
+          users[0],
+          BIG_NUMBER_1E18.mul(10000),
+          {
+            value: await arbitrumBridger["cost(uint256)"](10653818828),
+          },
+        ),
+      ).to.changeTokenBalance(sdl, users[0], BIG_NUMBER_1E18.mul(-10000))
+    })
+  })
+
+  describe("commitTransferOwnership", () => {
+    it(`Reverts when not called by the owner`, async () => {
+      await expect(
+        arbitrumBridger
+          .connect(signers[1])
+          .commitTransferOwnership(TEST_ADDRESS),
+      ).to.be.reverted
+    })
+
+    it(`Successfully sets futureOwner`, async () => {
+      await arbitrumBridger.commitTransferOwnership(TEST_ADDRESS)
+      expect(await arbitrumBridger.futureOwner()).to.eq(TEST_ADDRESS)
+    })
+  })
+
+  describe("acceptTransferOwnership", () => {
+    it(`Reverts when not called by the futureOwner`, async () => {
+      await expect(
+        arbitrumBridger.connect(signers[1]).acceptTransferOwnership(),
+      ).to.be.reverted
+    })
+
+    it(`Successfully transfers ownership to futureOwner`, async () => {
+      await arbitrumBridger.commitTransferOwnership(users[10])
+      await expect(
+        arbitrumBridger.connect(signers[10]).acceptTransferOwnership(),
+      )
+        .to.emit(arbitrumBridger, "TransferOwnership")
+        .withArgs(users[0], users[10])
+      expect(await arbitrumBridger.owner()).to.eq(users[10])
     })
   })
 })
