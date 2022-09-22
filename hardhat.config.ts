8f7a2af99a34b0077ae6fc03762f720caef05a60
Jongseung Lim <penandlim@gmail.com>
2022-09-22 00:49:44 -0400

    Update gas reporter and add a new ENV variable for controlling it

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -206,6 +206,7 @@ const config: HardhatUserConfig = {
   gasReporter: {
     currency: "USD",
     gasPrice: 21,
+    enabled: process.env.REPORT_GAS ? true : false,
   },
   mocha: {
     timeout: 200000,
