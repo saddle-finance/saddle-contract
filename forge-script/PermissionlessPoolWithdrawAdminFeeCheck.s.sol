a4095a41f8018c7cf730b0277b5aa3324e3c732e
Jongseung Lim <penandlim@gmail.com>
2022-10-05 20:08:31 -0400

    Add check for revert when called by non-owner

--- forge-script/PermissionlessPoolWithdrawAdminFeeCheck.s.sol
+++ forge-script/PermissionlessPoolWithdrawAdminFeeCheck.s.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "forge-std/Script.sol";
-import "@openzeppelin/contracts-4.4.0/token/ERC20/utils/SafeERC20.sol";
+import "./ScriptWithConstants.s.sol";
+import "../contracts/interfaces/IMasterRegistry.sol";
+import "@openzeppelin/contracts-4.4.0/token/ERC20/IERC20.sol";
 
 interface IPermissionlessSwap {
     function withdrawAdminFees() external payable;
@@ -14,23 +15,55 @@ interface IPermissionlessSwap {
     function getVirtualPrice() external view returns (uint256);
 }
 
-contract PermissionlessPoolWithdrawAdminFeeCheckScript is Script {
-    uint256 snapshot;
-    IPermissionlessSwap basePool =
-        IPermissionlessSwap(
-            address(0x1933B1D4A372Ecbc3713D92F875b330846ae6A2b)
-        );
-    IPermissionlessSwap metaPool =
-        IPermissionlessSwap(
-            address(0xE41389921Cc14e2159232bd6DeBc78924307e5a9)
-        );
-    IERC20 busd = IERC20(address(0x4Fabb145d64652a948d72533023f6E7A623C7C53));
-    IERC20 usdc = IERC20(address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48));
-    IERC20 usdt = IERC20(address(0xdAC17F958D2ee523a2206206994597C13D831ec7));
+interface IPoolRegistry {
+    /* Structs */
 
-    function setUp() public {
+    struct PoolData {
+        address poolAddress;
+        address lpToken;
+        uint8 typeOfAsset;
+        bytes32 poolName;
+        address targetAddress;
+        address[] tokens;
+        address[] underlyingTokens;
+        address basePoolAddress;
+        address metaSwapDepositAddress;
+        bool isSaddleApproved;
+        bool isRemoved;
+        bool isGuarded;
+    }
+
+    /* Functions */
+    function getPoolDataByName(bytes32 poolName)
+        external
+        view
+        returns (PoolData memory);
+}
+
+contract PermissionlessPoolWithdrawAdminFeeCheckScript is ScriptWithConstants {
+    uint256 snapshot;
+    IPermissionlessSwap basePool;
+    IPermissionlessSwap metaPool;
+
+    function setUp() public override {
+        super.setUp();
+
         // Choose a network and block number
         vm.createSelectFork("mainnet", 15684315);
+
+        IMasterRegistry mr = IMasterRegistry(
+            getDeploymentAddress("MasterRegistry")
+        );
+        IPoolRegistry pr = IPoolRegistry(
+            mr.resolveNameToLatestAddress("PoolRegistry")
+        );
+
+        basePool = IPermissionlessSwap(
+            (pr.getPoolDataByName("bUSD pool")).poolAddress
+        );
+        metaPool = IPermissionlessSwap(
+            (pr.getPoolDataByName("bUSD Metapool")).poolAddress
+        );
 
         // Create the snapshot to roll back to after each test
         snapshot = vm.snapshot();
@@ -58,9 +91,25 @@ contract PermissionlessPoolWithdrawAdminFeeCheckScript is Script {
         vm.revertTo(snapshot);
     }
 
+    function revertsWhenCalledByNonOwner() public {
+        // Impersonate a random account that is not an owner nor fee collector
+        address caller = address(0x00000000000000000000000000000000DeaDBeef);
+        require(caller != basePool.owner(), "Caller is owner");
+        require(caller != basePool.feeCollector(), "Caller is fee collector");
+
+        vm.startPrank(caller);
+
+        // Expect next tx to revert
+        vm.expectRevert();
+        basePool.withdrawAdminFees();
+        vm.stopPrank();
+        vm.revertTo(snapshot);
+    }
+
     function run() public {
         // https://github.com/foundry-rs/forge-std/blob/master/src/Vm.sol
         checkWithdrawAdminFees(basePool.owner());
         checkWithdrawAdminFees(basePool.feeCollector());
+        revertsWhenCalledByNonOwner();
     }
 }
