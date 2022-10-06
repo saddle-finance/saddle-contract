84c6bda386eeafe19d93d5d0ac8c258916d5bf64
Michael Daly <dalymike102@gmail.com>
2022-10-06 16:49:32 -0400

    script for deploying all contracts needed for permissionless pools on optimism

--- deploy/optimism/240_permissionless_deployments.ts
+++ deploy/optimism/240_permissionless_deployments.ts
@@ -10,72 +10,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, get, getOrNull, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
-  const poolRegistry = await getOrNull("PoolRegistry")
-  const masterRegistry = await getOrNull("MasterRegistry")
+  const shareProtocolFee = await getOrNull("ShareProtocolFee")
   const permissionlessSwap = await getOrNull("PermissionlessSwap")
   const permissionlessMetaSwap = await getOrNull("PermissionlessMetaSwap")
   const permissionlessDeployer = await getOrNull("PermissionlessDeployer")
 
-  // If not deployed deploy pool registry
-  if (poolRegistry == null) {
-    console.log("PoolRegistry not found, deploying")
-    await deploy("PoolRegistry", {
-      from: deployer,
-      log: true,
-      skipIfAlreadyDeployed: true,
-      args: [deployer, MULTISIG_ADDRESSES[await getChainId()]],
-    })
-    console.log(
-      `PoolRegistry deployed at ${(await get("PoolRegistry")).address}`,
+  // Get the MasterRegistry contract and address
+  const masterRegistry: MasterRegistry = await ethers.getContract(
+    "MasterRegistry",
+    deployer,
+  )
+  const masterRegistryAddress = masterRegistry.address
+
+  // see if master registry has a fee collector set
+  const feeCollectorName =
+    "0x466565436f6c6c6563746f720000000000000000000000000000000000000000"
+  try {
+    await masterRegistry.resolveNameToLatestAddress(feeCollectorName)
+  } catch (error) {
+    console.log("No fee collector set, setting now")
+    await masterRegistry.addRegistry(
+      feeCollectorName,
+      MULTISIG_ADDRESSES[await getChainId()],
     )
+    console.log("Successfully set fee collector")
   }
 
-  // If not deployed deploy master registry
-  if (masterRegistry == null) {
-    console.log("MasterRegistry not found, deploying")
-    await deploy("MasterRegistry", {
-      from: deployer,
-      log: true,
-      skipIfAlreadyDeployed: true,
-      args: [MULTISIG_ADDRESSES[await getChainId()]],
-    })
-
-    let masterRegistry = (await ethers.getContract(
-      "MasterRegistry",
-    )) as MasterRegistry
-    console.log(`MasterRegistry deployed at ${masterRegistry.address}`)
-
-    const batchCall = [
-      await masterRegistry.populateTransaction.addRegistry(
-        ethers.utils.formatBytes32String("PoolRegistry"),
-        (
-          await get("PoolRegistry")
-        ).address,
-      ),
-      await masterRegistry.populateTransaction.addRegistry(
-        ethers.utils.formatBytes32String("FeeCollector"),
-        MULTISIG_ADDRESSES[await getChainId()],
-      ),
-    ]
-
-    const batchCallData = batchCall
-      .map((x) => x.data)
-      .filter((x): x is string => !!x)
-
-    await execute(
-      "MasterRegistry",
-      { from: deployer, log: true },
-      "batch",
-      batchCallData,
-      true,
-    )
-  }
-
-  const masterRegistryAddress = (await get("MasterRegistry")).address
-
-  if (permissionlessSwap == null) {
-    console.log("PermissionlessSwap not found, deploying")
-    await deploy("PermissionlessSwap", {
+  // deploy ShareProtocolFeel if needed
+  if (shareProtocolFee == null) {
+    console.log("ShareProtocolFee not found, deploying")
+    await deploy("ShareProtocolFee", {
       from: deployer,
       log: true,
       skipIfAlreadyDeployed: true,
@@ -86,12 +50,32 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       },
     })
     console.log(
+      `ShareProtocolFee deployed at ${(await get("ShareProtocolFee")).address}`,
+    )
+  }
+
+  // deploy PermissionlessSwap if needed
+  if (permissionlessSwap == null) {
+    console.log("PermissionlessSwap not found, deploying")
+    await deploy("PermissionlessSwap", {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+      args: [masterRegistryAddress],
+      libraries: {
+        SwapUtils: (await get("SwapUtils")).address,
+        AmplificationUtils: (await get("AmplificationUtils")).address,
+        ShareProtocolFee: (await get("ShareProtocolFee")).address,
+      },
+    })
+    console.log(
       `PermissionlessSwap deployed at ${
         (await get("PermissionlessSwap")).address
       }`,
     )
   }
 
+  // deploy ShareProtocolFeel if needed
   if (permissionlessMetaSwap == null) {
     console.log("PermissionlessMetaSwap not found, deploying")
     await deploy("PermissionlessMetaSwap", {
@@ -112,6 +96,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     )
   }
 
+  // deploy PermissionlessDeployer if needed
   if (permissionlessDeployer == null) {
     console.log("PermissionlessDeployer not found, deploying")
     await deploy("PermissionlessDeployer", {
@@ -122,11 +107,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         MULTISIG_ADDRESSES[await getChainId()],
         (await get("MasterRegistry")).address,
         (await get("LPToken")).address,
-        (await get("PermissionlessSwapFlashLoan")).address,
-        (await get("PermissionlessMetaSwapFlashLoan")).address,
+        (await get("PermissionlessSwap")).address,
+        (await get("PermissionlessMetaSwap")).address,
         // Below needs to be a non-clone instance of the MetaswapDeposit Contract
         (
-          await get("MetaSwapDeposit")
+          await get("SaddleOptFRAXMetaPoolDeposit")
         ).address,
       ],
     })
@@ -136,6 +121,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       }`,
     )
 
+    // set target swaps for the permissionless deployer,
+    // PermissionlessDeployer to the master registry
+    console.log("Setting target swaps for PermissionlessDeployer")
+    console.log(
+      `PermissionlessSwap at: ${(await get("PermissionlessSwap")).address}`,
+    )
+    console.log(
+      `PermissionlessMetaSwap at: ${
+        (await get("PermissionlessMetaSwap")).address
+      }`,
+    )
+    await execute(
+      "PermissionlessDeployer",
+      { from: deployer, log: true },
+      "setTargetSwap",
+      (
+        await get("PermissionlessSwap")
+      ).address,
+    )
+
+    await execute(
+      "PermissionlessDeployer",
+      { from: deployer, log: true },
+      "setTargetMetaSwap",
+      (
+        await get("PermissionlessMetaSwap")
+      ).address,
+    )
+
+    console.log("Adding PermissionlessDeployer to MasterRegistry")
     await execute(
       "MasterRegistry",
       {
@@ -186,4 +201,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 }
 export default func
 func.tags = ["PermissionlessSwaps"]
+func.dependencies = [
+  "MasterRegistry",
+  "PoolRegistry",
+  "SwapUtils",
+  "AmplificationUtils",
+  "MetaSwapUtils",
+  "LPToken",
+]
 // func.skip = async (env) => (await env.getChainId()) == CHAIN_ID.MAINNET
