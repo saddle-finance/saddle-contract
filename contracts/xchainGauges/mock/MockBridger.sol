0ee8f52add166aa18e01997f154d8aa695a3d668
Jongseung (John) Lim <penandlim@gmail.com>
2022-10-12 14:22:05 -0400

    Update solidity versions to 0.8.17 and remove unused MockBridger functionalities (#741)
    * Update solidity versions to 0.8.17 and remove unused MockBridger
    functionalities
    
    * Remove tracer logging from root gauge test

--- contracts/xchainGauges/mock/MockBridger.sol
+++ contracts/xchainGauges/mock/MockBridger.sol
@@ -1,73 +1,27 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.6;
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts-4.4.0/token/ERC20/utils/SafeERC20.sol";
+import "../bridgers/Bridger.sol";
 
-import "@openzeppelin/contracts-4.2.0/token/ERC20/utils/SafeERC20.sol";
-
-interface IGatewayRouter {
-    function getGateWay(address _token) external view returns (address);
-
-    function outboundTransfer(
-        address _token,
-        address _to,
-        uint256 _amount,
-        uint256 _maxGas,
-        uint256 _gasPriceBid,
-        bytes calldata _data // _max_submission_cost, _extra_data
-    ) external payable;
-}
-
-contract MockBridger {
-    // consts
-    address private constant SDL = 0xf1Dc500FdE233A4055e25e5BbF516372BC4F6871;
-    // Arbitrum: L1 ERC20 Gateway
-    address private constant ARB_GATEWAY =
-        0xa3A7B6F88361F48403514059F1F16C8E78d60EeC;
-    address private constant ARB_GATEWAY_ROUTER =
-        0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef;
-    uint256 private constant MAX_UINT256 = 2**256 - 1;
-    address private constant ZERO_ADDRESS =
-        0x0000000000000000000000000000000000000000;
-
-    mapping(address => bool) public approved;
-
+contract MockBridger is Bridger {
     using SafeERC20 for IERC20;
 
-    constructor() {
-        // // approve token transfer to gateway
-        // IERC20 sdlToken = IERC20(SDL);
-        // // TODO: doesn't allow for safeApprove?
-        // assert(sdlToken.approve(ARB_GATEWAY, MAX_UINT256));
-        approved[SDL] = true;
-    }
+    constructor() {}
 
     function bridge(
         address _token,
         address _to,
         uint256 _amount
-    ) external payable {
-        // TODO: doesn't allow for safeTransferFrom?
-        assert(IERC20(_token).transferFrom(msg.sender, address(this), _amount));
-        if (_token != SDL && !approved[_token]) {
-            // TODO: doesn't allow for safeApprove?
-            assert(
-                IERC20(_token).approve(
-                    IGatewayRouter(ARB_GATEWAY_ROUTER).getGateWay(SDL),
-                    MAX_UINT256
-                )
-            );
-            approved[_token] = true;
-        }
-        // IGatewayRouter(ARB_GATEWAY_ROUTER).outboundTransfer{
-        //     value: gasLimit * gasPrice + maxSubmissionCost
-        // }(
-        //     _token,
-        //     _to,
-        //     _amount,
-        //     gasLimit,
-        //     gasPrice,
-        //     abi.encode(maxSubmissionCost, new bytes(0))
-        // );
+    ) external payable override {
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+    }
+
+    function cost() external view override returns (uint256) {
+        return 0;
+    }
+
+    function check(address) external view override returns (bool) {
+        return true;
     }
 }
