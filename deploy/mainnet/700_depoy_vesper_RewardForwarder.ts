1665f0eebc36405a1631ef5834bbe26714f23c9c
Michael Daly <dalymike102@gmail.com>
2022-10-14 15:09:35 -0400

    Deploy RewardForwarder with Vesper Liquidity Gauge

---
+++ deploy/mainnet/700_depoy_vesper_RewardForwarder.ts
@@ -0,0 +1,20 @@
+import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy, get } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await deploy("RewardForwarder", {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+    args: [
+      (
+        await get("LiquidityGaugeV5_SaddleFRAXBPVesperFRAXMetaPoolLPToken")
+      ).address,
+    ],
+  })
+}
+export default func
