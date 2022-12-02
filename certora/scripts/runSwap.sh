bc1305e31c4883a22ad60a34fda18311ae4cea1d
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2022-12-02 08:20:54 -0800

    add certoraRun for each batch

--- certora/scripts/runSwap.sh
+++ certora/scripts/runSwap.sh
@@ -5,6 +5,21 @@ else
     RULE="--rule $2"
 fi
 
+# certoraRun \
+#     certora/harness/SwapHarness.sol \
+#     certora/munged/LPToken.sol \
+#     --verify SwapHarness:certora/spec/Swap.spec \
+#     --structLink SwapHarness:lpToken=LPToken \
+#     --cache saddle \
+#     --loop_iter 2 \
+#     --settings -copyLoopUnroll=10,-t=1000,-mediumTimeout=40,-depth=50\
+#     $RULE \
+#     --send_only \
+#     --staging release/19Sep2022 \
+#     --optimistic_loop \
+#     --rule_sanity \
+#     --msg "Swap $1 $2 $3 $4 $5 $6"
+
 certoraRun \
     certora/harness/SwapHarness.sol \
     certora/munged/LPToken.sol \
@@ -12,31 +27,95 @@ certoraRun \
     --structLink SwapHarness:lpToken=LPToken \
     --cache saddle \
     --loop_iter 2 \
-<<<<<<< Updated upstream
-    --settings -copyLoopUnroll=10,-t=2000,-mediumTimeout=200,-depth=100 \
-=======
-    --settings -copyLoopUnroll=10,-t=1000,-mediumTimeout=40,-depth=50\
->>>>>>> Stashed changes
-    $RULE \
     --send_only \
     --staging release/19Sep2022 \
     --optimistic_loop \
     --rule_sanity \
-    --msg "Swap $1 $2 $3 $4 $5 $6"
-    # --staging yoav/array_of_structs_fix \
-    # --settings -enableEqualitySaturation=false \
+    --rules cantReinit onlyAdminCanSetSwapFees onlyAdminCanSetAdminFees pausedMeansLPMonotonicallyDecreases underlyingTokensDifferentInitialized \
+    --msg "Swap cantReinit onlyAdminCanSetSwapFees onlyAdminCanSetAdminFees pausedMeansLPMonotonicallyDecreases underlyingTokensDifferentInitialized" \
 
-#--rules cantReinit onlyAdminCanSetSwapFees onlyAdminCanSetAdminFees pausedMeansLPMonotonicallyDecreases underlyingTokensDifferentInitialized
+certoraRun \
+    certora/harness/SwapHarness.sol \
+    certora/munged/LPToken.sol \
+    --verify SwapHarness:certora/spec/Swap.spec \
+    --structLink SwapHarness:lpToken=LPToken \
+    --cache saddle \
+    --loop_iter 2 \
+    --send_only \
+    --staging release/19Sep2022 \
+    --optimistic_loop \
+    --rule_sanity \
+    --rules uninitializedImpliesZeroValue uninitializedImpliesRevert onlyRemoveLiquidityOneTokenDecreasesUnderlyingsOnesided removeLiquidityDoesntReduceAdminFees LPSupplyZeroMeansBalancesZero \
+    --msg "Swap uninitializedImpliesZeroValue uninitializedImpliesRevert onlyRemoveLiquidityOneTokenDecreasesUnderlyingsOnesided removeLiquidityDoesntReduceAdminFees LPSupplyZeroMeansBalancesZero" \
 
-#--rules uninitializedImpliesZeroValue uninitializedImpliesRevert onlyRemoveLiquidityOneTokenDecreasesUnderlyingsOnesided removeLiquidityDoesntReduceAdminFees LPSupplyZeroMeansBalancesZero
+certoraRun \
+    certora/harness/SwapHarness.sol \
+    certora/munged/LPToken.sol \
+    --verify SwapHarness:certora/spec/Swap.spec \
+    --structLink SwapHarness:lpToken=LPToken \
+    --cache saddle \
+    --loop_iter 2 \
+    --send_only \
+    --staging release/19Sep2022 \
+    --optimistic_loop \
+    --rule_sanity \
+    --rules monotonicallyIncreasingFees onlyAdminCanWithdrawFees pausedImpliesNoSingleTokenWithdrawal pausedImpliesTokenRatioConstant virtualPriceNeverZeroOnceLiquidityProvided \
+    --msg "Swap monotonicallyIncreasingFees onlyAdminCanWithdrawFees pausedImpliesNoSingleTokenWithdrawal pausedImpliesTokenRatioConstant virtualPriceNeverZeroOnceLiquidityProvided" \
 
-#--rules monotonicallyIncreasingFees onlyAdminCanWithdrawFees pausedImpliesNoSingleTokenWithdrawal pausedImpliesTokenRatioConstant virtualPriceNeverZeroOnceLiquidityProvided
+certoraRun \
+    certora/harness/SwapHarness.sol \
+    certora/munged/LPToken.sol \
+    --verify SwapHarness:certora/spec/Swap.spec \
+    --structLink SwapHarness:lpToken=LPToken \
+    --cache saddle \
+    --loop_iter 2 \
+    --send_only \
+    --staging release/19Sep2022 \
+    --optimistic_loop \
+    --rule_sanity \
+    --rules swappingCheckMinAmount onlyAddLiquidityCanInitialize addLiquidityCheckMinToMint swapAlwaysBeforeDeadline addLiquidityAlwaysBeforeDeadline \
+    --msg "Swap swappingCheckMinAmount onlyAddLiquidityCanInitialize addLiquidityCheckMinToMint swapAlwaysBeforeDeadline addLiquidityAlwaysBeforeDeadline" \
 
-#--rules swappingCheckMinAmount onlyAddLiquidityCanInitialize addLiquidityCheckMinToMint swapAlwaysBeforeDeadline addLiquidityAlwaysBeforeDeadline
+certoraRun \
+    certora/harness/SwapHarness.sol \
+    certora/munged/LPToken.sol \
+    --verify SwapHarness:certora/spec/Swap.spec \
+    --structLink SwapHarness:lpToken=LPToken \
+    --cache saddle \
+    --loop_iter 2 \
+    --send_only \
+    --staging release/19Sep2022 \
+    --optimistic_loop \
+    --rule_sanity \
+    --rules uninitializedImpliesZeroValueInv underlyingTokensDifferent LPsolvency underlyingsSolvency zeroTokenAZeroTokenB \
+    --msg "Swap uninitializedImpliesZeroValueInv underlyingTokensDifferent LPsolvency underlyingsSolvency zeroTokenAZeroTokenB" \
 
-#--rules uninitializedImpliesZeroValueInv underlyingTokensDifferent LPsolvency underlyingsSolvency zeroTokenAZeroTokenB
+certoraRun \
+    certora/harness/SwapHarness.sol \
+    certora/munged/LPToken.sol \
+    --verify SwapHarness:certora/spec/Swap.spec \
+    --structLink SwapHarness:lpToken=LPToken \
+    --cache saddle \
+    --loop_iter 2 \
+    --send_only \
+    --staging release/19Sep2022 \
+    --optimistic_loop \
+    --rule_sanity \
+    --rules uninitializedMeansUnderlyingsZero adminFeeNeverGreaterThanMAX swapFeeNeverGreaterThanMAX ifLPTotalSupplyZeroThenIndividualUnderlyingsZero ifSumUnderlyingsZeroLPTotalSupplyZero \
+    --msg "Swap uninitializedMeansUnderlyingsZero adminFeeNeverGreaterThanMAX swapFeeNeverGreaterThanMAX ifLPTotalSupplyZeroThenIndividualUnderlyingsZero ifSumUnderlyingsZeroLPTotalSupplyZero" \
 
-#--rules uninitializedMeansUnderlyingsZero adminFeeNeverGreaterThanMAX swapFeeNeverGreaterThanMAX ifLPTotalSupplyZeroThenIndividualUnderlyingsZero ifSumUnderlyingsZeroLPTotalSupplyZero
+certoraRun \
+    certora/harness/SwapHarness.sol \
+    certora/munged/LPToken.sol \
+    --verify SwapHarness:certora/spec/Swap.spec \
+    --structLink SwapHarness:lpToken=LPToken \
+    --cache saddle \
+    --loop_iter 2 \
+    --send_only \
+    --staging release/19Sep2022 \
+    --optimistic_loop \
+    --rule_sanity \
+    --rules removeLiquidityAlwaysBeforeDeadline LPtotalSupplyZeroWhenUninitialized \
+    --msg "Swap removeLiquidityAlwaysBeforeDeadline LPtotalSupplyZeroWhenUninitialized" \
 
-#--rules removeLiquidityAlwaysBeforeDeadline LPtotalSupplyZeroWhenUninitialized
 
