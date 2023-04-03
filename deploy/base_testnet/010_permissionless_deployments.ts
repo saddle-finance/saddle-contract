76300e029042bc9fb67b63e5de6cf42ae9b7ca44
Michael Daly <dalymike102@gmail.com>
2023-04-03 15:13:06 -0400

    updated to newest permissionless deploy util

--- deploy/base_testnet/010_permissionless_deployments.ts
+++ deploy/base_testnet/010_permissionless_deployments.ts
@@ -1,12 +1,12 @@
 import dotenv from "dotenv"
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { deployPermissionlessPoolComponents } from "../deployUtils"
+import { deployPermissionlessPoolComponentsV2 } from "../deployUtils"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   dotenv.config()
 
-  await deployPermissionlessPoolComponents(hre)
+  await deployPermissionlessPoolComponentsV2(hre)
 }
 export default func
 func.tags = ["PermissionlessSwaps"]
